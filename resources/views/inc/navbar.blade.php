667766e9e9f9f4e4f07111bf01316556c9f03721
Chad Quilter <chadquilter@gmail.com>
2018-03-22 21:45:20 -0500

    update header look

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -16,9 +16,8 @@
       </ul>
 </nav>
 <br>
-
-<img alt="{{ config('app.name', 'Space City Preparatory') }}" src="/images/scps_logo.png" height="140">
 <div>
+<img alt="{{ config('app.name', 'Space City Preparatory') }}" src="/images/scps_logo.png" height="140">
   <h1 style="color: white; text-shadow: 2px 2px 4px #000000;">{{ config('app.name', 'Space City Preparatory') }}</h1>
 </div>
 <br>
