6262a356b8bb02eb11706bde03422e81e4749e46
Chad Quilter <chadquilter@gmail.com>
2018-03-23 10:12:06 -0500

    update header look

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
   <div class="container">
-    <br>
-    @include('inc.pagelabel')
-    <br>
     <div class="card bg-secondary border border-dark rounded image_display_r">
       <div class="card-img-top">
         @include('inc.jobimagecarosel')
