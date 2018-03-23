1baf6f96d6f5b36731025441a562c48995dcf45e
Chad Quilter <chadquilter@gmail.com>
2018-03-23 11:46:50 -0500

    update header look

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -30,7 +30,7 @@
           <h1 style="text-align: left; color: white; text-shadow: 2px 2px 4px #000000;">{{ config('app.name', 'Space City Preparatory') }}</h1>
         </div>
         <!-- Split dropup button -->
-        <div class="col" style="align: right">
+        <div class="col col-4" style="align: right">
           <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             English
           </button>
