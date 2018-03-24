d88835463ab3f6ba39b8ec5fcb54bc3f025b485c
Chad Quilter <chadquilter@gmail.com>
2018-03-23 23:01:03 -0500

    update header look

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -59,7 +59,10 @@
     </div>
   </div>
   <br>
+
   <nav class="navbar navbar-expand-md navbar-dark border border-dark main-color">
+    <div style="background-color: blue;">
+    </div>
     <div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
