3a7ea73ce4c522a42ca9263d26c803ad9ddade1d
Chad Quilter <chadquilter@gmail.com>
2018-03-23 22:41:22 -0500

    update header look

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -60,7 +60,7 @@
   </div>
   <br>
   <div class="row">
-    <div class="col image_display_r" styel="width: 80%">
+    <div class="col image_display_r" style="width: 80%">
       <img alt="{{ config('app.name', 'Space City Preparatory') }}" src="/images/blue-ribbon.png">
     </div>
   </div>
