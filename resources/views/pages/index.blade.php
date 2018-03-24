159cedc2f1a912b03c9afcb13be28093b348e342
Chad Quilter <chadquilter@gmail.com>
2018-03-23 22:20:46 -0500

    update header look

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="container">
-    <div class="card">
+    <div class="card alt-color">
       <div class='card-img-top'>
         @include('inc.jobimagecarosel')
       </div>
