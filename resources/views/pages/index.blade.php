ceb01c7dbde9cf837b4791a5b8aea86bb477a3fd
Chad Quilter <chadquilter@gmail.com>
2018-03-23 22:19:25 -0500

    update header look

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,15 +8,21 @@
       </div>
       <div class="card-body alt-color">
         <div class="jumbotron">
-          <p class="lead">
-            Values and Excellence
+          <div class="row">
+            <div class="col">
+              <p class="lead">
+                Values and Excellence
 
-            California Crosspoint Academy is like no other school in the world, and we have the international reputation and experience to prove it.
-            Our comprehensive and challenging academics place us among the top schools in the nation and prepares our students to become tomorrow’s global leaders.
-            Our Christian philosophy provides the moral and spiritual compass for purposeful and peaceful living.
+                California Crosspoint Academy is like no other school in the world, and we have the international reputation and experience to prove it.
+                Our comprehensive and challenging academics place us among the top schools in the nation and prepares our students to become tomorrow’s global leaders.
+                Our Christian philosophy provides the moral and spiritual compass for purposeful and peaceful living.
 
-            I invite you to spend a few moments to find out more about the programs and people who  comprise the CCA Rams community.  Better yet, schedule a phone call or a visit so you can personally see, hear, and witness what makes our middle school and high school such a transformative and uplifting place to live, learn, and grow.
-          </p>
+                I invite you to spend a few moments to find out more about the programs and people who  comprise the CCA Rams community.  Better yet, schedule a phone call or a visit so you can personally see, hear, and witness what makes our middle school and high school such a transformative and uplifting place to live, learn, and grow.
+              </p>
+            </div>
+            <div class="col">
+              <div class="calandar">Placeholder</div>
+            </div>
         </div>
       </div>
     </div>
