8b4603cfc6376e7ef431a84155c13f6400fb9ed4
Chad Quilter <chadquilter@gmail.com>
2018-03-23 22:56:54 -0500

    update header look

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,13 +10,13 @@
 
           <div class="row">
             <div class="col">
+              <h1 class="lead">Values and Excellence</h1>
               <p class="lead">
-                <h1 class="lead">Values and Excellence</h1>
-                <br>
                 Space City Preparatory School is like no other school in the world, and we have the international reputation and experience to prove it.
                 Our comprehensive and challenging academics place us among the top schools in the nation and prepares our students to become tomorrow’s global leaders.
                 Our philosophy provides the morals and knowledge for purposeful and peaceful living.
-                <br>
+              </p>
+              <p class="lead">
                 I invite you to spend a few moments to find out more about the programs and people who comprise the SCPS community.
                 Better yet, schedule a phone call or a visit so you can personally see, hear, and witness what makes our middle school and high school such a transformative and uplifting place to live, learn, and grow.
               </p>
