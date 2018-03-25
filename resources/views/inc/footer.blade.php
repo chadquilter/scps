531222f240340cea2c0fe150ab6c7cb5ee7a76ed
Chad Quilter <chadquilter@gmail.com>
2018-03-25 12:16:52 -0500

    add changes to footer and index blade

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,33 +1,46 @@
 <footer class="footer navbar-dark text-white">
   <div class="foot-container main-color">
     <div class="footer-top-1">
-      <center>
-        <div class="row">
-          <div class="img-1-footer">
-            <img src="/img/place-holder.jpeg"/>
-          </div>
-          <div class="img-1-footer">
-            <img src="/img/place-holder.jpeg"/>
-          </div>
-          <div class="img-1-footer">
-            <img src="/img/place-holder.jpeg"/>
-          </div>
-          <div class="img-1-footer">
-            <img src="/img/place-holder.jpeg"/>
-          </div>
-        </div>
-      </center>
+
     </div>
     <div class="footer-text" styel="width:50%;">
       <center>
-        <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
-          2018,  {{ config('app.name', 'SCPS') }}
-        </p>
-        <hr>
-        <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
-          P.O. Box 590743 Houston, TX 77259 U.S<br>
-          Tele: (832) 205-2160 <br>
-        </p>
+        <div class="row">
+          <div class="col">
+            <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
+              2018,  {{ config('app.name', 'SCPS') }}
+            </p>
+            <hr>
+            <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
+              P.O. Box 590743 Houston, TX 77259 U.S<br>
+              Tele: (832) 205-2160 <br>
+            </p>
+          </div>
+          <div class="col">
+              <p class="lead">
+                this is a test
+              </p>
+          </div>
+          <div class="col">
+              <p class="lead">
+                this is a test
+              </p>
+          </div>
+          <div class="col">
+              <p class="lead">
+                this is a test
+              </p>
+          </div>
+          <div class="col">
+              <p class="lead">
+                this is a test
+              </p>
+          </div>
+          <div class="col">
+              <p class="lead">
+                this is a test
+              </p>
+          </div>
       </center>
     </div>
     <div class="footer-badges">
