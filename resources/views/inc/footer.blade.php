f7484d21389ebc8ce55be0d49a1c884fc2148df2
Chad Quilter <chadquilter@gmail.com>
2018-03-26 22:22:04 -0500

    update css carosel

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,9 +1,6 @@
 <footer class="footer navbar-dark text-white">
-  <div class="foot-container">
-    <div class="footer-top-1">
-    </div>
+  <div class="foot-container main-color">
     <div class="footer-text" styel="width:50%;">
-      <center>
         <div class="row">
           <div class="col">
             <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
@@ -16,36 +13,34 @@
             </p>
           </div>
           <div class="col">
-              <p class="lead">
-                Quick Links
-              </p>
-              <br>
-              <ul class="nav nav-pills nav-fill main-color">
-                <li class="nav-item">
-                  <a class="nav-link active" href="#">New Students Apply</a>
-                </li>
-              </ul>
-              <br>
-              <ul class="nav nav-pills nav-fill main-color">
-                <li class="nav-item">
-                  <a class="nav-link active" href="#">Powerschool</a>
-                </li>
-              </ul>
+            <p class="lead">
+              Quick Links
+            </p>
+            <br>
+            <ul class="nav nav-pills nav-fill main-color">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">New Students Apply</a>
+              </li>
+            </ul>
+            <br>
+            <ul class="nav nav-pills nav-fill main-color">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Powerschool</a>
+              </li>
+            </ul>
           </div>
           <div class="col">
-              <p class="lead">
-                Find Us on Chat-Face
-              </p>
+            <p class="lead">
+              Find Us on Chat-Face
+            </p>
           </div>
           <div class="col">
-              <p class="lead">
-                Contact Us:
-              </p>
+            <p class="lead">
+              Contact Us:
+            </p>
           </div>
-      </center>
-    </div>
-    <div class="footer-badges">
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </footer>
