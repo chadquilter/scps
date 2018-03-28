c2362cb77923cbe77ce47ed7c8b95767c33764b5
Chad Quilter <chadquilter@gmail.com>
2018-03-27 20:39:37 -0500

    update index page

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,45 +1,7 @@
 <footer class="footer navbar-dark text-white">
   <div class="foot-container main-color">
     <div class="footer-text" styel="width:50%;">
-        <div class="row">
-          <div class="col">
-            <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
-              2018,  {{ config('app.name', 'SCPS') }}
-            </p>
-            <hr>
-            <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
-              P.O. Box 590743 Houston, TX 77259 U.S<br>
-              Tele: (832) 205-2160 <br>
-            </p>
-          </div>
-          <div class="col">
-            <p class="lead">
-              Quick Links
-            </p>
-            <br>
-            <ul class="nav nav-pills nav-fill main-color">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">New Students Apply</a>
-              </li>
-            </ul>
-            <br>
-            <ul class="nav nav-pills nav-fill main-color">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Powerschool</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col">
-            <p class="lead">
-              Find Us on Chat-Face
-            </p>
-          </div>
-          <div class="col">
-            <p class="lead">
-              Contact Us:
-            </p>
-          </div>
-        </div>
+      
     </div>
   </div>
 </footer>
