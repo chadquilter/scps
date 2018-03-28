6f805080d94f2c672178ddfaed792d09c8dd3c21
Chad Quilter <chadquilter@gmail.com>
2018-03-28 01:57:12 -0500

    update index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -60,7 +60,7 @@
             </div>
             <br>
             <div class="row">
-              <div class="col image_display_r">
+              <div class="col">
                 <div class="main-color text-light" style="padding: 18px 20px 0px;">
                   <h1>Calendar</h1>
                 </div>
