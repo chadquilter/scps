d8ef4e26f535d4335874df1dc9dc8a9346d33938
Chad Quilter <chadquilter@gmail.com>
2018-03-28 17:40:06 -0500

    update index page layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -129,12 +129,16 @@
               </div>
               <div class="card-img-caption">this is a test 1</div>
             </div>
+          </div>
+          <div class="col">
             <div class="card">
               <div class="card-img-top">
                 <img src="/images/btn-calendar2.png" border="0">
               </div>
               <div class="card-img-caption">this is a test 2</div>
             </div>
+          </div>
+          <div class="col">
             <div class="card">
               <div class="card-img-top">
                 <img src="/images/btn-calendar2.png" border="0">
