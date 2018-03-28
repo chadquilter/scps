5b1472564c306f73a9b93f8f9d4393ecf7c26d30
Chad Quilter <chadquilter@gmail.com>
2018-03-27 21:37:09 -0500

    update index page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -30,7 +30,7 @@
     <div class="col col-auto">
       <img alt="{{ config('app.name', 'Space City Preparatory') }}" src="/images/scps_logo.png" height="140">
     </div>
-    <div class="col col-8">
+    <div class="col col-sm-10">
       <div class="row">
         <div class="col">
           <h1 class="display-4" style="text-align: left; color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">{{ config('app.name', 'Space City Preparatory') }}</h1>
