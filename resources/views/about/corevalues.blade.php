198f2b69defe0073a88ed713f8e4da9cb5382da6
Chad Quilter <chadquilter@gmail.com>
2018-04-01 12:07:00 -0500

    add new blades for subpages

--- resources/views/about/corevalues.blade.php
+++ resources/views/about/corevalues.blade.php
@@ -7,28 +7,28 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-            <p class="lead">
+
               <h1>{{ __('Scope of our Mission')}}</h1>
-              <br>
+              <p class="lead">
               {{ __('We want to reach all who can benefit from our school.
               We want to develop each student to their full potential.
               We want to emphasize spiritual results over academic achievement.
               We want to be faithful to our task and trust God to give the increase.')}}
-              <br>
+              </p>
               <h1>{{ __('Governance and Operations')}}</h1>
-              <br>
+              <p class="lead">
               {{ __('We want to practice servant leadership: teachers serve students; administrators serve teachers.
               We want to make what is best for our students a top priority.
               We want to emphasize the spirit of the law over the letter of the law.
               We want to emphasize ‘who we are’ is more important than ‘what we do’.
               We want to place overall, long-term benefits over expediency.')}}
-              <br>
+              </p>
               <h1>{{ __('Methodology')}}</h1>
-              <br>
+              <p class="lead">
               {{ __('We want to make sure the teacher is the key to the classroom.
               We want to equip our teachers to teach: spiritual health, professional training, tools and resources, supportive and positive environment.
               We want to work in partnership with parents and emphasize comprehensive development: academic, physical, psychological, social, cultural.')}}
-            </p>
+              </p>
           </div>
           <div class="col">
             <div class="main-color text-light">
