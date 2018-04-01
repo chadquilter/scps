bc6d461de19ea834ebbe3f4956b595ddffd70b95
Chad Quilter <chadquilter@gmail.com>
2018-04-01 11:56:00 -0500

    add new blades for subpages

--- resources/views/about/corevalues.blade.php
+++ resources/views/about/corevalues.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
   <div class="container">
-    <div class="card alt-color">
+    <div class="card">
       <div class="card-header">{{ __('CORE VALUES') }}</div>
-      <div class="card-body">
+      <div class="card-body alt-color">
         <div class="row">
           <div class="col">
             <p class="lead">
