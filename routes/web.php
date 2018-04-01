abc31f3df62c0c9b538fc24e0b891a3c65c05f31
Chad Quilter <chadquilter@gmail.com>
2018-04-01 06:05:30 -0500

    add new blades for subpages

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ Auth::routes([
 ]);
 
 Route::resource('about', 'AboutController');
-Route::get('/schoolhistory', 'AboutController@schoolhistory');
-Route::get('/corevalues', 'AboutController@corevalues');
-Route::get('/eslr', 'AboutController@eslr');
-Route::get('/testemony', 'AboutController@testemony');
-Route::get('/vision', 'AboutController@vision');
+Route::get('about/schoolhistory', 'AboutController@schoolhistory');
+Route::get('about/corevalues', 'AboutController@corevalues');
+Route::get('/about/eslr', 'AboutController@eslr');
+Route::get('/about/testemony', 'AboutController@testemony');
+Route::get('/about/vision', 'AboutController@vision');
 
 Route::resource('academics', 'AcademicsController');
 Route::resource('admissions', 'AdmissionController');
