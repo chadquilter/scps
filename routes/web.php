2809f2669ab483c49e1f60a462cc6045b9abcd35
Chad Quilter <chadquilter@gmail.com>
2018-04-01 03:37:07 -0500

    add zh language files

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', 'PagesController@index');
 
 
 Route::get('/about', 'AboutController@index');
-Route::get('/academics', 'AcademicsController@index');
-Route::get('/admissions', 'AdmissionsController@index');
-Route::get('/studentlife', 'StudentController@index');
-Route::get('/international', 'InternationalController@index');
-Route::get('/contact', 'ContactController@index');
-Route::get('/documents', 'DocumentController@index');
+//Route::get('/academics', 'AcademicsController@index');
+//Route::get('/admissions', 'AdmissionsController@index');
+//Route::get('/studentlife', 'StudentController@index');
+//Route::get('/international', 'InternationalController@index');
+//Route::get('/contact', 'ContactController@index');
+//Route::get('/documents', 'DocumentController@index');
