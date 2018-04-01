bdfef37fbfc98443f6a4723976b7beebcccf5dc6
Chad Quilter <chadquilter@gmail.com>
2018-04-01 18:25:22 -0500

    add mini contact form

--- resources/views/international/admissionfees.blade.php
+++ resources/views/international/admissionfees.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="container">
     <div class="card">
-      <div class="card-header"><h2 class="display-2">{{ __('International Admission & Fees') }}</h2></div>
+      <div class="card-header"><h3 class="display-3">{{ __('International Admission & Fees') }}</h3></div>
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
