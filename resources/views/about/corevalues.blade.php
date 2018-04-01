d72234a87e3b8178d6bd303b56dfa4d576d52ef9
Chad Quilter <chadquilter@gmail.com>
2018-04-01 10:43:09 -0500

    add new blades for subpages

--- resources/views/about/corevalues.blade.php
+++ resources/views/about/corevalues.blade.php
@@ -2,51 +2,53 @@
 
 @section('content')
   <div class="container alt-color">
-    <div class="row ">
+    <div class="row">
       <div class="col">
         <div class="card">
           <div class="card-header">{{ __('CORE VALUES') }}</div>
 
           <div class="card-body">
             <p class="lead">
-Scope of our Mission
-We want to reach all who can benefit from our school.
-We want to develop each student to their full potential.
-We want to emphasize spiritual results over academic achievement.
-We want to be faithful to our task and trust God to give the increase.
-Governance and Operations
-We want to practice servant leadership: teachers serve students; administrators serve teachers.
-We want to make what is best for our students a top priority.
-We want to emphasize the spirit of the law over the letter of the law.
-We want to emphasize ‘who we are’ is more important than ‘what we do’.
-We want to place overall, long-term benefits over expediency.
-Methodology
-We want to make sure the teacher is the key to the classroom.
-We want to equip our teachers to teach: spiritual health, professional training, tools and resources, supportive and positive environment.
-We want to work in partnership with parents and emphasize comprehensive development: academic, physical, psychological, social, cultural.
-</p>
+              Scope of our Mission
+              We want to reach all who can benefit from our school.
+              We want to develop each student to their full potential.
+              We want to emphasize spiritual results over academic achievement.
+              We want to be faithful to our task and trust God to give the increase.
+              Governance and Operations
+              We want to practice servant leadership: teachers serve students; administrators serve teachers.
+              We want to make what is best for our students a top priority.
+              We want to emphasize the spirit of the law over the letter of the law.
+              We want to emphasize ‘who we are’ is more important than ‘what we do’.
+              We want to place overall, long-term benefits over expediency.
+              Methodology
+              We want to make sure the teacher is the key to the classroom.
+              We want to equip our teachers to teach: spiritual health, professional training, tools and resources, supportive and positive environment.
+              We want to work in partnership with parents and emphasize comprehensive development: academic, physical, psychological, social, cultural.
+            </p>
           </div>
         </div>
       </div>
       <div class="col">
-      <div class="row">
-        <div class="col">
-          <div class="main-color text-light" style="padding: 18px 20px 0px;">
-            <h1>{{__('Links')}}</h1>
-          </div>
+        <div class="row">
           <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <a href="/admissions/application">
-                  <img src="/images/parent-information-night.jpg" border="0">
-                </a>
-                <hr class="lead">
+            <div class="main-color text-light" style="padding: 18px 20px 0px;">
+              <h1>{{__('Links')}}</h1>
+            </div>
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                    <ul>
+                      <li>test</li>
+                      <li>test2</li>
+                      <li>test3</li>
+                    </ul>
+                  <hr class="lead">
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   </div>
 @endsection
