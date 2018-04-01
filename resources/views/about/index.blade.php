07f3555daf54db77143f5cf15a0fba678b75fd09
Chad Quilter <chadquilter@gmail.com>
2018-04-01 04:43:07 -0500

    add zh language files

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -2,9 +2,16 @@
 
 @section('content')
   <div class="container">
-      <div class="row justify-content-center">
-          <div class="col-md-8">
-  about about
-</div>
-</div>
-</div>
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">{{ __('Login') }}</div>
+
+          <div class="card-body">
+            about about
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
