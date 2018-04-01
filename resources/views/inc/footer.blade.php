f7f46cd573a311e92ccec9038429d4d825c425bb
Chad Quilter <chadquilter@gmail.com>
2018-04-01 16:51:57 -0500

    add mini contact form

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class="footer-text" style="width:50%;">
         <div class="row">
-          <div class="col col-6">
+          <div class="col">
             <p class="lead" style="color: #FAEBD7; text-shadow: 2px 2px 4px #000000;">
               2018,  {{ config('app.name', 'SCPS') }}
             </p>
@@ -16,7 +16,7 @@
               Tele: (832) 205-2160 <br>
             </p>
           </div>
-          <div class="col col-2">
+          <div class="col">
             <p class="lead">
               Quick Links
             </p>
@@ -27,7 +27,7 @@
               </li>
             </ul>
           </div>
-          <div class="col col-6">
+          <div class="col">
             @include('contact.contactmini')
           </div>
         </div>
