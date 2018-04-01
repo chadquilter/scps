7baab8060f4c37f873fc281d69475c600330ab4c
Chad Quilter <chadquilter@gmail.com>
2018-04-01 02:52:29 -0500

    add zh language files

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,13 @@
 */
 
 
+Route::get('locale', function () {
+    return \App::getLocale();
+});
 Route::get('locale/{locale}', function ($locale) {
     \Session::put('locale', $locale);
     return redirect()->back();
 });
-
 
 Route::get('/', 'PagesController@index');
 
