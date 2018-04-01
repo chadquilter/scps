725c96a0310fbc21214ea9eb46e30ccdc4746d30
Chad Quilter <chadquilter@gmail.com>
2018-04-01 15:50:48 -0500

    add contact form

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -9,32 +9,33 @@
 <div id="after_submit"></div>
 <form id="contact_form" action={{ url('/contact/store')  }} method="POST" enctype="multipart/form-data">
   @csrf
-  <div class="row">
+  <div class="form=group">
     <label class="required" for="name"><strong>Your name: (required)</strong></label>
     <br>
     <input id="name" class="input" name="name" type="text" value="" size="30" /><br />
     <span id="name_validation" class="error_message"></span>
   </div>
-  <div class="row">
+  <div class="form=group">
     <label class="required" for="email"><strong>Your email: (required)</strong></label>
     <br>
     <input id="email" class="input" name="email" type="text" value="" size="30" /><br />
     <span id="email_validation" class="error_message"></span>
   </div>
-  <div class="row">
+  <div class="form=group">
     <label class="required" for="phone"><strong>Your phone: (required)</strong></label>
     <br>
     <input id="phone" class="input" name="phone" type="text" value="" size="30" /><br />
     <span id="phone_validation" class="error_message"></span>
   </div>
-  <div class="row">
+  <div class="form=group">
     <label class="required" for="message"><string>Message: (required)</strong></label>
     <br>
     <textarea id="description" class="input" name="description" rows="7" cols="30"></textarea><br />
     <span id="description_validation" class="error_message"></span>
   </div>
-
+  <div class="form=group">
     <input id="submit_button" type="submit" value="Send email" />
+  </div>
 </form>
 </div>
 </div>
