81499bf941c52cfa5b2f03991e82833744d51304
Chad Quilter <chadquilter@gmail.com>
2018-04-01 16:07:35 -0500

    add contact form

---
+++ resources/views/inc/sidebarcontacts.blade.php
@@ -0,0 +1,30 @@
+<div class="main-color text-light sidebar_pad">
+  <h1>{{__('Our Map')}}</h1>
+  map
+</div>
+<br>
+<div class="main-color text-light sidebar_pad">
+  <h1>{{__('Contact Information')}}</h1>
+  map
+</div>
+<div class="card">
+  <div class="card-body">
+    <ul>
+      <li>{{__('Space City Preparatory School')}}</li>
+      <li>{{__('P.O. Box 590743 ')}}</li>
+      <li>{{__('Houston, TX')}}</li>
+      <li>{{__('77259 U.S.')}}</li>
+      <li>{{__('Phone Number: (832) 205-2160')}}</li>
+    </ul>
+    <hr class="lead">
+    <ul>
+      <p class="lead"><strong>{{__('Admissions:')}}</strong>
+        {{__('If you have questions about admissions, visit our admissions page or contact our Admissions Directore at (832) 205-2160')}}
+      </p>
+      <p class="lead"><strong>{{__('General Informations:')}}</strong>Email</p>
+      <p class="lead"><strong>{{__('Admissions:')}}</strong>Email</p>
+      <p class="lead"><strong>{{__('Job Opportunities:')}}</strong>Email</p>
+      <p class="lead"><strong>{{__('Media & Marketing:')}}</strong>Email</p>
+    <hr class="lead">
+  </div>
+</div>
