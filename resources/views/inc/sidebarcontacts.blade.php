d9f6670c5ac7b60e59940cbd62004eeb3738c502
Chad Quilter <chadquilter@gmail.com>
2018-04-01 16:20:59 -0500

    add contact form

--- resources/views/inc/sidebarcontacts.blade.php
+++ resources/views/inc/sidebarcontacts.blade.php
@@ -1,11 +1,19 @@
 <div class="main-color text-light sidebar_pad">
   <h1>{{__('Our Map')}}</h1>
-  map
+</div>
+<div class="card">
+  <div class="card-body">
+    <div class="mapouter">
+      <div class="gmap_canvas">
+        <iframe width="200" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=P.O. Box 590743,houston,TX,78259=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      </div>
+      <style>.mapouter{overflow:hidden;height:200px;width:200px;}.gmap_canvas {background:none!important;height:200px;width:200px;}</style>
+    </div>
+  </div>
 </div>
 <br>
 <div class="main-color text-light sidebar_pad">
   <h1>{{__('Contact Information')}}</h1>
-  map
 </div>
 <div class="card">
   <div class="card-body">
