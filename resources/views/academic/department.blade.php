e37de0d9a22dfa9bc33e684a8b06800b25b792e5
Chad Quilter <chadquilter@gmail.com>
2018-04-02 16:46:25 -0500

    add deparm

--- resources/views/academic/department.blade.php
+++ resources/views/academic/department.blade.php
@@ -7,7 +7,7 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-              <div href="#" id="departmentButton" class="btn btn-primary btn-block">English</div>
+              <input type="button" id="departmentButton" class="btn btn-primary btn-block">English</div>
               <div class="departmentDiv">
                 @include('academic.departments.english')
               </div>
@@ -19,15 +19,6 @@
       </div>
     </div>
   </div>
-  <div class="mod_wrap">
-    <div class="t_mod_box">
-        <a href="#" class="edittopic" name="send">Toggle</a>
-    </div>
-
-    <div class="t_edit_cont">
-       Show hide content inside here... 1
-    </div>
-</div>
   <script>
   //gets next available node from button and hides it
   $( "#departmentButton" ).click(function() {
