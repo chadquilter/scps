4a9475e51355347f49ebd4a996f2441916d67bf3
Chad Quilter <chadquilter@gmail.com>
2018-04-02 12:43:21 -0500

    add sidelink array for labels and links

--- resources/views/inc/sidebarlinks.blade.php
+++ resources/views/inc/sidebarlinks.blade.php
@@ -1,6 +1,6 @@
 
 <div class="main-color text-light sidebar_pad shadow_only">
-  <h1>{{__('Section Bookmarks')}}</h1>
+  <h1>{{__('Bookmarks')}}</h1>
 </div>
 <div class="card">
   <div class="card-body shadow_only">
