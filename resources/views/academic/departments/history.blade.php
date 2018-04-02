198922062f675d564ca3084f2b9595d625089ba0
Chad Quilter <chadquilter@gmail.com>
2018-04-02 17:26:33 -0500

    add departments

---
+++ resources/views/academic/departments/history.blade.php
@@ -0,0 +1,40 @@
+<h1>{{ __('ESL')}}</h1>
+<p class="lead">
+{{ __('ESL is designed to introduce English reading, writing, and oral language skills to English Language Learners. Students will study phonics and practice pronunciation, develop reading comprehension skills, and develop oral fluency. Students will build their English vocabulary and receive assistance in academic language that will help them succeed in all content areas.')}}
+</p>
+<h1>{{ __('ADVANCED ESL')}}</h1>
+<p class="lead">
+{{ __('This course is for Advanced English Language Learners. Students continue to refine skills in reading, writing, speaking, listening, and grammar. Grammar instruction emphasizes the editing process for personal habitual error patterns each student makes in the English language. Note-taking skills, academic vocabulary and literary analysis are emphasized. Subject matter includes the analysis and comprehension of poetry, short stories, novels, film, art and cultural awareness.')}}
+</p>
+<h1>{{ __('ENGLISH I')}}</h1>
+<p class="lead">
+{{ __('This is a college prep course that will develop strong literary skills by equipping students to be critical thinkers, analytical writers, comprehensive readers, and articulate speakers. Selected novels, dramas, short stories, poems, and articles will be analyzed in terms of purpose, rhetoric, and style. Writing skills will be advanced, and students will research, write and deliver a persuasive speech as well as practice narrative, persuasive, and expository writing, with primary emphasis placed ')}}
+</p>
+<h1>{{ __('ENGLISH HONERS')}}</h1>
+<p class="lead">
+{{ __('This course is designed to access a variety of types of literature and make reading a transformative habit. In the process students will be trained to Read Well, Write Well, and Think Well. In light of this goal, we will be doing a variety of assignments, types of literature, writings, presentations, and activities. We will be reading several novels, discuss poetry, Shakespeare, and develop the skills necessary to have a good discussion.')}}
+</p>
+<h1>{{ __('ENGLISH II HONORS')}}</h1>
+<p class="lead">
+{{ __('This course integrates world literature with composition and prepares students for AP Language and AP Literature. Selected novels, dramas, short stories, and articles that encompass the scope of world literature will be read and analyzed in terms of purpose, rhetoric, and style. Writing skills will be advanced, and students will research, write and deliver a persuasive speech.')}}
+</p>
+<h1>{{ __('American Literature')}}</h1>
+<p class="lead">
+{{ __('This course addresses some of the major literary movements of the time, including the Colonial Period (1600s), the Age of Reason (1700s), the Romantic Period (early 1800s), Realism (mid 1800s), and Naturalism (post-Civil War). Students will read a variety of historically-relevant fiction and non-fiction short stories, articles, essays, novels, and plays which explore themes, build cultural literacy, connect history with literature.')}}
+</p>
+<h1>{{ __('British Literature')}}</h1>
+<p class="lead">
+{{ __('This course is a survey course which encompasses major prose and poetry of English writers spanning the periods from Anglo-Saxon to modern/contemporary. The goal is to acquire an understanding of the ideas and literary genres that define these periods and to build a foundation for advanced studies in English literature. All readings will be considered in a literary and historical context so that an understanding will be gained of the historical, cultural and philosophical influences.')}}
+</p>
+<h1>{{ __('Advanced Composition & Literature')}}</h1>
+<p class="lead">
+{{ __('This course is designed to engage students in college level reading, discussion, and analysis of novels, plays, poetry, non-fiction essays, and literary criticism. Students will learn how to read interesting, thought-provoking texts with enough depth to produce interesting, engaging discussions, write in a clear, intelligent manner, and grow as people.')}}
+</p>
+<h1>{{ __('AP Language')}}</h1>
+<p class="lead">
+{{ __('Students will analyze, at a college level, rhetorical strategies and techniques in a variety of non-fiction texts and begin to incorporate these skills in their own writing and speaking, with a goal toward confident and effective communication.')}}
+</p>
+<h1>{{ __('AP Literature')}}</h1>
+<p class="lead">
+{{ __('This course is designed to engage students in close reading, discussion, and analysis of novels, plays, poetry, non-fiction essays and literary criticism. While the course devotes significant time to preparing students for the AP Literature exam, the class seeks to look beyond the exam itself toward more lasting purposes, namely, developing a solid and enduring worldview lens through which to interpret difficult texts.')}}
+</p>
