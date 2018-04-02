97a482e2eb1fb6b8f109d7ba3f7b1a56315d0be5
Chad Quilter <chadquilter@gmail.com>
2018-04-02 12:17:44 -0500

    add sidelink array for labels and links

--- resources/views/inc/sidebarlinks.blade.php
+++ resources/views/inc/sidebarlinks.blade.php
@@ -7,7 +7,7 @@
     <ul>
       @foreach($sidelinks as $sidelink)
         <li>
-          <a href="/{{$sidelink}}" class="btn btn-primary">{{$sidelink}}</a>
+          <a href="/{{$sidelink[link]}}" class="btn btn-primary">{{$sidelink[label]}}</a>
         </li>
       @endforeach
     </ul>
