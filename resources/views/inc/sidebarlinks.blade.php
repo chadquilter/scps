2d77e263b6058a10c7da449e93e83bf5af608b84
Chad Quilter <chadquilter@gmail.com>
2018-04-02 12:31:22 -0500

    add sidelink array for labels and links

--- resources/views/inc/sidebarlinks.blade.php
+++ resources/views/inc/sidebarlinks.blade.php
@@ -1,9 +1,9 @@
-<div class="main-color text-light sidebar_pad image_display_r">
+<div class="image_display_r">
+<div class="main-color text-light sidebar_pad">
   <h1>{{__('Quick Links')}}</h1>
 </div>
-<br>
 <div class="card">
-  <div class="card-body image_display_r">
+  <div class="card-body">
     <ul>
       @foreach($sidelinks as $sidelink)
           <a href="/{{$sidelink["link"]}}" class="btn btn-primary btn-block">{{$sidelink["label"]}}</a>
@@ -12,4 +12,5 @@
     </ul>
     <hr class="lead">
   </div>
+</div>
 </div>
