bd72fce8dd7cbf5e8f75be34dce1341303c51f3c
Chad Quilter <chadquilter@gmail.com>
2018-04-02 13:36:41 -0500

    remove login navlink

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -129,7 +129,7 @@
           </div>
         </li>
         <li class="nav-item {{ ( Request::route()->getName() == 'contact.index') ? 'active' : '' }}">
-          <a class="nav-link" href="/contact">{{__('Contact Us')}}</a>
+          <a class="nav-link" href="/contact">{{__('Contact')}}</a>
         </li>
       </ul>
 
