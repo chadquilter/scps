797546e16d748132fedc8f440b3df3fed8c39446
Chad Quilter <chadquilter@gmail.com>
2018-04-02 23:54:21 -0500

    add image gallery sections

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -51,7 +51,7 @@
               fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));</script>
 
-            <div class="fb-page" data-href="https://www.facebook.com/spacecityschool" data-tabs="timeline" data-width="300" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/spacecityschool" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/spacecityschool">Space City Preparatory School</a></blockquote></div>
+            <div class="fb-page" data-href="https://www.facebook.com/spacecityschool" data-tabs="timeline" data-width="300" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/spacecityschool" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/spacecityschool">Space City Preparatory School</a></blockquote></div>
           </div>
           <div class="col">
             @include('contact.contactmini')
