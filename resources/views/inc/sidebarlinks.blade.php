f6158cbf12e400130941e9abdc2b0541ab7c24ab
Chad Quilter <chadquilter@gmail.com>
2018-04-02 19:20:18 -0500

    add departments

--- resources/views/inc/sidebarlinks.blade.php
+++ resources/views/inc/sidebarlinks.blade.php
@@ -6,7 +6,7 @@
   <div class="card-body shadow_only">
     <ul>
       @foreach($sidelinks as $sidelink)
-          <a class="btn btn-outline-dark btn-block {{ ( \Request::url() == url($sidelink["link"]) ) ? 'active' : '' }}" href="{{ url($sidelink["link"]) }}">{{$sidelink["label"]}}</a>
+          <a class="btn btn-outline-alert btn-block {{ ( \Request::url() == url($sidelink["link"]) ) ? 'active' : '' }}" href="{{ url($sidelink["link"]) }}">{{$sidelink["label"]}}</a>
           <br>
       @endforeach
     </ul>
