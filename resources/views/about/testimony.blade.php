7d33f0e7b3d75533281ade3dcba79c976162834b
Chad Quilter <chadquilter@gmail.com>
2018-04-03 13:55:56 -0500

    commit all

--- resources/views/about/testimony.blade.php
+++ resources/views/about/testimony.blade.php
@@ -8,21 +8,22 @@
         <div class="row">
           <div class="col">
               <p class="lead">
-                <h1 class="display-1">“</h1>
+                <h3 class="display-1">“</h3>
               {{ __('...unlike most high schools, SCPS was like a home. Not only because of the circumstances that caused me to see the place as my home physically, but more because of the people, the teachers, the students, even the administration, that made me feel welcomed and loved, a feeling that can only be so real in a family.')}}
               </p>
               <hr class="style-two">
 
               <p class="lead">
-                <h1 class="display-1">“</h1>
+                <h3 class="display-1">“</h3>
               {{ __('I am grateful for all the support and love that I had during my time there. A few of my favorite teachers and influential people in my life are still at Space City Preparatory! I hope that they continue teaching and influencing the next generation,')}}
               </p>
               <hr class="style-two">
 
               <p class="lead">
-                <h1 class="display-1">“</h1>
+                <h3 class="display-1">“</h3>
               {{ __('the teachers, the students, even the administration, that made me feel welcomed and loved, a feeling that can only be so real in a family.')}}
               </p>
+              <hr class="style-two">
           </div>
           <div class="col">
             @include('inc.sidebarlinks')
