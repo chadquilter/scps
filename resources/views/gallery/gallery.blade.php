2c123c886dca8561ef8a7f519ccfde7e24959549
Chad Quilter <chadquilter@gmail.com>
2018-04-02 23:30:51 -0500

    add image gallery sections

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -1,7 +1,7 @@
 <div class="gallery">
   <div class="card">
     <div class="card-header">
-      <h1 class="display-1">{{__($title)}}</h1>
+      <h2 class="display-2">{{__($title)}}</h2>
     </div>
     <div class="card-body alt-color">
       <div class="card image_display_r">
