e08ab276cb970c65f4e6ebf1dc089ed506816611
Chad Quilter <chadquilter@gmail.com>
2018-04-02 23:22:01 -0500

    add image gallery sections

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Storage;
 
 class GalleryController extends Controller
 {
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Service;
 use Storage;
 
 class PagesController extends Controller
