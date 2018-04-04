90756a75503ecb85ba4a66918661db9e4fb043cc
Chad Quilter <chadquilter@gmail.com>
2018-04-03 19:08:26 -0500

    commit all

--- resources/views/academic/administration.blade.php
+++ resources/views/academic/administration.blade.php
@@ -9,15 +9,11 @@
           <div class="col">
               <h1>{{ __('Administrator')}}</h1>
               <p class="lead">
-              {{ __('')}}
+              {{ __('1812')}}
               </p>
-              <h1>{{ __('Governance and Operations')}}</h1>
+              <h1>{{ __('Creativtiy')}}</h1>
               <p class="lead">
-              {{ __('We want to practice servant leadership: teachers serve students; administrators serve teachers.
-              We want to make what is best for our students a top priority.
-              We want to emphasize the spirit of the law over the letter of the law.
-              We want to emphasize ‘who we are’ is more important than ‘what we do’.
-              We want to place overall, long-term benefits over expediency.')}}
+              {{ __('1812! never surrender, never give up!')}}
               </p>
               <h1>{{ __('Methodology')}}</h1>
               <p class="lead">
