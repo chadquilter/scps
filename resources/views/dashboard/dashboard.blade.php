1a3525cfd7ad190b2cb995e93b69aaf377c0a0f7
Chad Quilter <chadquilter@gmail.com>
2018-04-04 17:33:00 -0500

    add dashboard models, controller and blades

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,27 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.front')
 
 @section('content')
-<div class="container">
-  <div class="jumbotron">
-    <h4 class="display-4">Dashboard </h4>
+  <div class="container">
+    <div class="card">
+      <div class='card-body alt-color'>
+        <div class="jumbotron">
+          <h4 class="display-4">Dashboard </h4>
 
-    <div class="card mx-auto">
-      <br>
-      <div class="card-body">
-        <p class="lead"><strong> Welcome {{ Auth::user()->name }}</strong></p>
-        <p class="lead"><strong> {{ $dt }} </strong></p>
-        @include('inc.dashmenu')
-        <hr>
-        @include('inc.messages')
-        <br>
-        @if (session('status'))
-          <div class="alert alert-success">
-            {{ session('status') }}
+          <div class="card mx-auto">
+            <br>
+            <div class="card-body">
+              <p class="lead"><strong> Welcome {{ Auth::user()->name }}</strong></p>
+              <p class="lead"><strong> {{ $dt }} </strong></p>
+              @include('inc.dashmenu')
+              <hr>
+              @include('inc.messages')
+              <br>
+              @if (session('status'))
+                <div class="alert alert-success">
+                  {{ session('status') }}
+                </div>
+              @endif
+            </div>
           </div>
-        @endif
-
+        </div>
       </div>
     </div>
   </div>
-</div>
 @endsection
