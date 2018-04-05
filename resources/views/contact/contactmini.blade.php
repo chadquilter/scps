0e0a1171f2ebfdd5cf4bb683cf93c1314cd89117
Chad Quilter <chadquilter@gmail.com>
2018-04-04 23:56:39 -0500

    commit updates to contact mini

--- resources/views/contact/contactmini.blade.php
+++ resources/views/contact/contactmini.blade.php
@@ -7,21 +7,21 @@
         <form id="contact_form_mini" action={{ url('/contact/store')  }} method="POST" enctype="multipart/form-data">
           @csrf
           <div class="form-group row">
-            <label for="name" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Your Name:') }}</strong></label>
+            <label for="name" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Name:') }}</strong></label>
             <div class="col-md-6">
               <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
             </div>
           </div>
 
           <div class="form-group row">
-            <label for="email" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Your Email:') }}</strong></label>
+            <label for="email" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Email:') }}</strong></label>
             <div class="col-md-6">
               <input id="text" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
             </div>
           </div>
 
           <div class="form-group row">
-            <label for="phone" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Your Phone:') }}</strong></label>
+            <label for="phone" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Phone:') }}</strong></label>
             <div class="col-md-6">
               <input id="phone" type="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
             </div>
