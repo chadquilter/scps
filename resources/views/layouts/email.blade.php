98cb01e9024e7fc579da06d23706b73a85196e0f
Chad Quilter <chadquilter@gmail.com>
2018-04-05 01:29:11 -0500

    add sub for handling redirect

---
+++ resources/views/layouts/email.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="CNQ Software Services">
+    <meta name="keywords" content="{{__('meta_keywords')}}"/>
+    <meta name="description" content="{{__('page_description')}}"/>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{__('Space City Preparatory School') }}</title>
+    <!-- Styles -->
+    <link href="{{ /url() }}/css/app.css" rel="stylesheet">
+    <!-- js -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+  <main role="main">
+    <div class="body-container">
+      @yield('content')
+    </div>
+  </main>
+</body>
+</html>
