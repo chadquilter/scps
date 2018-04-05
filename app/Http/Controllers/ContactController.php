582173942dc86e631a4e13a139243d436ef029e9
Chad Quilter <chadquilter@gmail.com>
2018-04-04 19:48:05 -0500

    add dashboard models, controller and blades

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactMail;
-use App\Http\Requests\storeContactPost;
+use App\Http\Requests\storeContactPost as storeContactPost;
 
 class ContactController extends Controller
 {
