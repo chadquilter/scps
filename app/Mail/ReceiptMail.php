770855a822010d00c9b016dac893a69979189505
Chad Quilter <chadquilter@gmail.com>
2018-04-05 02:36:29 -0500

    add sub for handling redirect

---
+++ app/Mail/ReceiptMail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use App\Contact;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Carbon\Carbon;
+
+class ReceiptMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $contact;
+
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+
+        $date = Carbon::now()->toDateTimeString();
+        return $this->view('emails.recipt')
+            ->with([
+                'contactName' => $this->contact->name,
+                'contactPhone' => $this->contact->phone,
+                'contactEmail' => $this->contact->email,
+                'contactNotes' => $this->contact->notes,
+                'contactDate' => $date,
+            ]);
+    }
+}
