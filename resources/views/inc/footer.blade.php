299c79a777da22b2b5a55eedfef9a1a53ea338d8
Chad Quilter <chadquilter@gmail.com>
2018-04-05 03:33:08 -0500

    add sub for handling redirect

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -49,7 +49,8 @@
               js = d.createElement(s); js.id = id;
               js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
               fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
+            }(document, 'script', 'facebook-jssdk'));
+            document.body.scrollTop = document.documentElement.scrollTop = 0;</script>
 
             <div class="fb-page" data-href="https://www.facebook.com/spacecityschool" data-tabs="timeline" data-width="300" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/spacecityschool" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/spacecityschool">Space City Preparatory School</a></blockquote></div>
           </div>
