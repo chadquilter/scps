cccaf36da4fc4f73d010612e023efd6a618f43de
Chad Quilter <chadquilter@gmail.com>
2018-04-05 03:14:16 -0500

    add sub for handling redirect

--- resources/views/contact/contactmini.blade.php
+++ resources/views/contact/contactmini.blade.php
@@ -7,28 +7,28 @@
         <form id="contact_form_mini" action={{ url('/contact/store')  }} method="POST" enctype="multipart/form-data">
           @csrf
           <div class="form-group row">
-            <label for="name" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Name:') }}</strong></label>
+            <label for="name" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Name:') }}</strong></label><br>
             <div class="col-md-6">
               <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
             </div>
           </div>
 
           <div class="form-group row">
-            <label for="email" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Email:') }}</strong></label>
+            <label for="email" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Email:') }}</strong></label><br>
             <div class="col-md-6">
               <input id="text" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
             </div>
           </div>
 
           <div class="form-group row">
-            <label for="phone" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Phone:') }}</strong></label>
+            <label for="phone" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Phone:') }}</strong></label><br>
             <div class="col-md-6">
               <input id="phone" type="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
             </div>
           </div>
 
           <div class="form-group row">
-            <label for="notes" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Message:') }}</strong></label>
+            <label for="notes" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Message:') }}</strong></label><br>
             <div class="col-md-6">
               <textarea id="notes" rows="7" cols="40" class="form-control{{ $errors->has('notes') ? ' is-invalid' : '' }}" name="notes" required autofocus>{{ old('notes') }}</textarea>
             </div>
