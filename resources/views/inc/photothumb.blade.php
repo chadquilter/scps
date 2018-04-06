9dea84c2532d4946973d0473c3be8d2b28891a17
Chad Quilter <chadquilter@gmail.com>
2018-04-05 21:54:32 -0500

    set col width to small for mobile

--- resources/views/inc/photothumb.blade.php
+++ resources/views/inc/photothumb.blade.php
@@ -9,14 +9,14 @@
   <div class="col">
         <div class="img-container">
           <a href="/art-gallery">
-            <img src="/images/gallery_icons/Art-135x110.jpg" alt="Avatar" class="img-image" style="width:100%">
+            <img src="/images/gallery_icons/Art-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
   </div>
   <div class="col">
     <div class="img-container">
       <a href="/classroom-gallery">
-        <img src="/images/gallery_icons/Classrooms-135x110.jpg" alt="Avatar" class="img-image" style="width:100%">
+        <img src="/images/gallery_icons/Classrooms-135x110.jpg" alt="Avatar" class="img-image">
       </a>
     </div>
 
@@ -24,7 +24,7 @@
   <div class="col">
         <div class="img-container">
           <a href="/electives-gallery">
-            <img src="/images/gallery_icons/Electives-135x110.jpg" alt="Avatar" class="img-image" style="width:100%">
+            <img src="/images/gallery_icons/Electives-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
 
@@ -32,14 +32,14 @@
   <div class="col">
         <div class="img-container">
           <a href="/sports-gallery">
-            <img src="/images/gallery_icons/Sports-135x110.jpg" alt="Avatar" class="img-image" style="width:100%">
+            <img src="/images/gallery_icons/Sports-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
   </div>
   <div class="col">
         <div class="img-container">
           <a href="/student-life-gallery">
-            <img src="/images/gallery_icons/Studentlife-135x110.jpg" alt="Avatar" class="img-image" style="width:100%">
+            <img src="/images/gallery_icons/Studentlife-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
 
