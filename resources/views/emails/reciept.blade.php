2250ec038b89749577fc9af918bd563731d89df7
Chad Quilter <chadquilter@gmail.com>
2018-04-05 21:00:56 -0500

    set col width to small for mobile

--- resources/views/emails/reciept.blade.php
+++ resources/views/emails/reciept.blade.php
@@ -2,20 +2,11 @@
 
 @section('content')
   <div class="container">
-
-    <div class="row">
-      <div class="col col-auto">
-        <img alt="{{ config('app.name', 'Space City Preparatory') }}" src="{{ asset('/images/scps_logo_sm.png')}} " height="140">
-      </div>
-      <div class="col col-sm-10">
-        <div class="row">
-          <div class="col">
-            <h1 class="display-4" style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000;">{{__('Space City Preparatory School')}}</h1>
-            <h5 style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000; font-style: oblique;">{{__('~ Educating The World.')}}</h5>
-          </div>
-        </div>
-      </div>
-    </div>
+    <br>
+    <img alt="{{ config('app.name', 'Space City Preparatory') }}" src="{{ asset('/images/scps_logo_sm.png')}} " height="140">
+    <br>
+    <h1 class="display-4" style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000;">{{__('Space City Preparatory School')}}</h1>
+    <h5 style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000; font-style: oblique;">{{__('~ Educating The World.')}}</h5>
     <hr class="style-two">
     <div class="card">
       <div class='card-body alt-color'>
