2edad29dd26e3c10582dc68e3f8e404107587726
Chad Quilter <chadquilter@gmail.com>
2018-04-05 21:14:45 -0500

    set col width to small for mobile

---
+++ resources/views/inc/sidecalendar.blade.php
@@ -0,0 +1,20 @@
+<div class="row">
+  <div class="col">
+    <div class="main-color text-light sidebar_pad">
+      <h1>{{__('Calendar')}}</h1>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-body image_display_r">
+          <div class="row">
+            <img src="/images/event_sidebar.png" border="0">Events
+          </div>
+          <hr>
+          <div class="row">
+            <img src="/images/sport_sidebar.png" border="0">Sports
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
