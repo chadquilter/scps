d019ddedf933f0d8fa5f561e2e190a9cbede7ee4
Chad Quilter <chadquilter@gmail.com>
2018-04-09 11:50:49 -0500

    update dept page js for new sub departments

--- resources/views/academic/departments/history.blade.php
+++ resources/views/academic/departments/history.blade.php
@@ -7,9 +7,10 @@
 
 <p class="lead">{{ __('History review and analysis goes beyond causes and effects but leads to the study and understanding of cultures, movements, motivations, and critical moments. Students gain an accurate and broad perspective of history by studying all of time, engaging in simulations and documentary projects, and examining every civilization. Students learn to apply their studies by recognizing patterns and historical trends to anticipate and understand today’s current events.')}}</p>
 <hr class="style-two">
-<div class="row">
-<input type="button" id="subdepartmentButton" class="btn btn-danger btn-block subdpt-button" value="{{__('World History')}}">
+
+<input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('World History')}}">
 <div class="card subdepartmentDiv">
+
 <div class="card-header">
 <h1>{{ __('World History')}}</h1>
 </div>
@@ -19,10 +20,8 @@
 </p>
 </div>
 </div>
-</div>
-
-<div class="row">
-<input type="button" id="subdepartmentButton" class="btn btn-danger btn-block subdpt-button" value="{{__('U.S. History')}}">
+<br>
+<input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('U.S. History')}}">
 <div class="card subdepartmentDiv">
 <div class="card-header">
 <h1>{{ __('U.S. History')}}</h1>
@@ -33,70 +32,101 @@
 </p>
 </div>
 </div>
-</div>
-
-<input type="button" id="subdepartmentButton" class="btn btn-primary btn-block subdpt-button" value="{{__('AP U.S. History')}}">
-<div class="subdepartmentDiv">
+<br>
+<input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('AP U.S. History')}}">
+<div class="card subdepartmentDiv">
+<div class="card-header">
 <h1>{{ __('AP U.S. History')}}</h1>
+</div>
+<div class="card-body">
 <p class="lead">
 {{ __('This is a challenging course that is equivalent to a freshman college course where students can earn college credit. Students will survey American history including the Age of Exploration, Colonial America, the American Revolution, Creation of the Nation, the Constitution and Federalism, Age of Reform, Sectionalism v. Nationalism, Manifest Destiny, the Civil War, Reconstruction, Industrialization, Urbanization, the Gilded Age, Populism, Imperialism, the Progressive Era, World War I, the Great Depression, the New Deal World War II, the Cold War, Civil Rights Movement, the Vietnam War, Watergate, etc. Concurrently, students will analyze and synthesize history in writing and class discussions.')}}
 </p>
 </div>
-
+</div>
+<br>
 <input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('Government')}}">
-<div class="subdepartmentDiv">
+<div class="card subdepartmentDiv">
+<div class="card-header">
 <h1>{{ __('Government')}}</h1>
+</div>
+<div class="card-body">
 <p class="lead">
 {{ __('This course actively connects civil government to the everyday lives of students. Some of the key issues explored are the responsibilities of government vs. individual liberty, centralized vs. localized government, and first principles vs. evolving authorization. A large portion of the class is spent studying the structure, organization, and practices of the United States government. The expected outcome of this course is that students will be inspired and prepared to be active citizens of the United States.')}}
 </p>
 </div>
-
+</div>
+<br>
 <input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('AP Government')}}">
-<div class="subdepartmentDiv">
+<div class="card subdepartmentDiv">
+<div class="card-header">
 <h1>{{ __('AP Government')}}</h1>
+</div>
+<div class="card-body">
 <p class="lead">
 {{ __('This course serves as a college level introduction to the U.S. national government explaining both who governs, and to what end it is done, by investigating the politics of policymaking and examining the government institutions which make these type of policies and the impact they have on people throughout the U.S. The class begins with a study of our constitutional underpinnings before an extensive study of each of the three branches of government including the federal bureaucracy. Finally, the course studies civil rights, public policy, and the political process.')}}
 </p>
 </div>
+</div>
 <br>
 <input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('Economics')}}">
-<div class="subdepartmentDiv">
+<div class="card subdepartmentDiv">
+<div class="card-header">
 <h1>{{ __('Economics')}}</h1>
+</div>
+<div class="card-body">
 <p class="lead">
 {{ __('This course explores Economics as the science of choice. People make countless decisions every day, and economics attempts to explain the reasoning behind these decisions through the perspective of individuals, firms, as well as the government. Complex concepts such as supply and demand, price controls, profitability, and government regulation in terms of understandable real world situations will be addressed.')}}
 </p>
 </div>
-
-<input type="button" id="subdepartmentButton" class="btn btn-outline-dark btn-block subdpt-button" value="{{__('AP Microeconomics')}}">
-<div class="subdepartmentDiv">
+</div>
+<br>
+<input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('AP Microeconomics')}}">
+<div class="card subdepartmentDiv">
+<div class="card-header">
 <h1>{{ __('AP Microeconomics')}}</h1>
+</div>
+<div class="card-body">
 <p class="lead">
 {{ __('This course covers material which includes the functions and operations of free market economies. Students must be comfortable with graphing concepts, as much of the instruction and analysis will be based on graphs. The course will begin with basic economic concepts such as scarcity, opportunity costs and the functions of economic systems. The majority of the course will deal with the nature and function of product markets; topics will include: the market system, including product markets and resource markets; government activity and the affects on market mechanisms; type of firms and their market functions; and international economics and the world economy.')}}
 </p>
 </div>
-
-<input type="button" id="subdepartmentButton" class="btn btn-outline-dark btn-block subdpt-button" value="{{__('AP European History')}}">
-<div class="subdepartmentDiv">
+</div>
+<br>
+<input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('AP European History')}}">
+<div class="card subdepartmentDiv">
+<div class="card-header">
 <h1>{{ __('AP European History')}}</h1>
+</div>
+<div class="card-body">
 <p class="lead">
 {{ __('This college level course covers the period from 1450 to 2000 AD. All arenas of human endeavor are included: politics, economics, art & architecture, philosophy, culture, medicine, war, industry, famous persons, events and literature. The emphasis of the course is on learning the key ideas, events and people who shaped Western Civilization. Historical periods include the Renaissance, Reformation, Age of Exploration, Age of Absolutism, Scientific Revolution, the Enlightenment, the French Revolution & Napoleon, the Industrial Revolution, the Era of National Unification, the Age of “Progress”, Imperialism, World War I, World War II, the Immediate Post-war Era, the Era of European Unification, and Globalization.')}}
 </p>
 </div>
-
-<input type="button" id="subdepartmentButton" class="btn btn-outline-dark btn-block subdpt-button" value="{{__('U.S. History Sheltered')}}">
-<div class="subdepartmentDiv">
+</div>
+<br>
+<input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('U.S. History Sheltered')}}">
+<div class="card subdepartmentDiv">
+<div class="card-header">
 <h1>{{ __('U.S. History Sheltered')}}</h1>
+</div>
+<div class="card-header">
 <p class="lead">
 {{ __('This class is designed to cover the same curriculum as other U.S. History classes, but it utilizes techniques specially designed to help English Language Learners. A goal of this class is to see how one period of time has influenced and shaped succeeding eras. Historical periods and topics include: Age of Exploration, Colonial America, the American Revolution, Creation of the Nation, the Constitution and Federalism, Age of Reform, Sectionalism v. Nationalism, Manifest Destiny, the Civil War, Reconstruction, etc. These periods and topics will be examined to determine whether the US upheld the founding ideals of democracy, equality, opportunity, rights, and liberty.')}}
 </p>
 </div>
+</div>
 
-<input type="button" id="subdepartmentButton" class="btn btn-outline-dark btn-block subdpt-button" value="{{__('AP Psychology')}}">
-<div class="subdepartmentDiv">
+<input type="button" id="subdepartmentButton" class="btn btn-outline-primary btn-block subdpt-button" value="{{__('AP Psychology')}}">
+<div class="card subdepartmentDiv">
+  <div class="card-header">
 <h1>{{ __('AP Psychology')}}</h1>
+</div>
+<div class="card-body">
 <p class="lead">
 {{ __('This course is designed to be a comprehensive first-year college course that introduces students to the systematic and scientific study of the behavior and mental processes of both human beings and animals. Students will be exposed to psychological facts, principles, and phenomena associated with each of the major subfields within psychology. They will also learn about the ethics and methods of psychological research and practice.')}}
 </p>
+</div>
 </div>
 
   </div>
