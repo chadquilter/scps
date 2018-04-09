8c3db119bdf0595067e71f9e90156618b0eee410
Chad Quilter <chadquilter@gmail.com>
2018-04-09 18:34:28 -0500

    add add domestic application

--- resources/views/international/acceleratedenglish.blade.php
+++ resources/views/international/acceleratedenglish.blade.php
@@ -7,26 +7,9 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-              <h1>{{ __('Scope of our Mission')}}</h1>
+              <h1>{{ __('Accelerated English Camp')}}</h1>
               <p class="lead">
-              {{ __('We want to reach all who can benefit from our school.
-              We want to develop each student to their full potential.
-              We want to emphasize spiritual results over academic achievement.
-              We want to be faithful to our task and trust God to give the increase.')}}
-              </p>
-              <h1>{{ __('Governance and Operations')}}</h1>
-              <p class="lead">
-              {{ __('We want to practice servant leadership: teachers serve students; administrators serve teachers.
-              We want to make what is best for our students a top priority.
-              We want to emphasize the spirit of the law over the letter of the law.
-              We want to emphasize ‘who we are’ is more important than ‘what we do’.
-              We want to place overall, long-term benefits over expediency.')}}
-              </p>
-              <h1>{{ __('Methodology')}}</h1>
-              <p class="lead">
-              {{ __('We want to make sure the teacher is the key to the classroom.
-              We want to equip our teachers to teach: spiritual health, professional training, tools and resources, supportive and positive environment.
-              We want to work in partnership with parents and emphasize comprehensive development: academic, physical, psychological, social, cultural.')}}
+              {{ __('Accelerated English Camp is specifically designed to help students transition to an American high school on an academic, social, and personal level. The intensive four-week program will provide students with instruction on American culture and norms, strengthen their conversational English, and increase their academic vocabulary in order to prepare them for entering an American high school shortly thereafter.')}}
               </p>
           </div>
           <div class="col">
