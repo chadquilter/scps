a99cf2056688efa67ef6da784cfa26f3dd92f7d3
Chad Quilter <chadquilter@gmail.com>
2018-04-09 18:25:13 -0500

    add add domestic application

--- resources/views/international/academy.blade.php
+++ resources/views/international/academy.blade.php
@@ -8,6 +8,7 @@
         <div class="row">
           <div class="col">
               <h1>{{ __('The Academy')}}</h1>
+              <hr class="style-two">
               <p class="lead">
               {{ __('The Academy at Space City Preparatory is a transition high school program that provides intensive English instruction while allowing students to learn new content using special instructional program to develop English proficiency while continuing academic instruction in core subjects. SCPS has successfully used this curriculum and approach for years in Beijing to teach students American curriculum while at the same time strengthening their English comprehension and speaking skills.')}}
               </p>
