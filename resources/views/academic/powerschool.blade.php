9a01255560d31546b682bae7198e76a227fa83cf
Chad Quilter <chadquilter@gmail.com>
2018-04-09 15:01:54 -0500

    add electives array listing

--- resources/views/academic/powerschool.blade.php
+++ resources/views/academic/powerschool.blade.php
@@ -9,24 +9,15 @@
           <div class="col">
               <h1>{{ __('Scope of our Mission')}}</h1>
               <p class="lead">
-              {{ __('We want to reach all who can benefit from our school.
-              We want to develop each student to their full potential.
-              We want to emphasize spiritual results over academic achievement.
-              We want to be faithful to our task and trust God to give the increase.')}}
+              {{ __('PowerSchool is the fastest-growing, most widely used web-based student information system in today’s academic world. PowerSchool enables SCPS educators to make timely decisions that impact student performance while creating a collaborative environment for parents, teachers and students to work together in preparing 21st century learners for the future.')}}
               </p>
               <h1>{{ __('Governance and Operations')}}</h1>
               <p class="lead">
-              {{ __('We want to practice servant leadership: teachers serve students; administrators serve teachers.
-              We want to make what is best for our students a top priority.
-              We want to emphasize the spirit of the law over the letter of the law.
-              We want to emphasize ‘who we are’ is more important than ‘what we do’.
-              We want to place overall, long-term benefits over expediency.')}}
+              {{ __('PowerSchool provides the SCPS staff with the full range of features needed, in addition to portals for teachers, parents, and students.  With internet access and the touch of a keyboard, parents and students can read the daily announcements, check grades, upcoming assignments, attendance, medical records, lunch balances, email teachers and much more!')}}
               </p>
               <h1>{{ __('Methodology')}}</h1>
               <p class="lead">
-              {{ __('We want to make sure the teacher is the key to the classroom.
-              We want to equip our teachers to teach: spiritual health, professional training, tools and resources, supportive and positive environment.
-              We want to work in partnership with parents and emphasize comprehensive development: academic, physical, psychological, social, cultural.')}}
+              {{ __('Current parents and students may login at any time.')}}
               </p>
           </div>
           <div class="col">
