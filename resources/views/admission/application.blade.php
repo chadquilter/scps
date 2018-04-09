fbc7dd3e3e6c307baa289d845db1ef543d94a837
Chad Quilter <chadquilter@gmail.com>
2018-04-09 17:21:38 -0500

    add add domestic application

--- resources/views/admission/application.blade.php
+++ resources/views/admission/application.blade.php
@@ -7,27 +7,12 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-              <h1>{{ __('Scope of our Mission')}}</h1>
+              <h1>{{ __('Middle School & High School Registration:')}}</h1>
               <p class="lead">
-              {{ __('We want to reach all who can benefit from our school.
-              We want to develop each student to their full potential.
-              We want to emphasize spiritual results over academic achievement.
-              We want to be faithful to our task and trust God to give the increase.')}}
+              {{ __('Thank you for your interest in Space City Preparatory. Your child’s education and training are very important to us and we want to assist you in making the right choice. Every school is different and not every learning environment is best suited for every child. California Crosspoint Academy offers an excellent and comprehensive college – preparatory academic program in a highly structured but supportive learning community. We encourage you to visit many schools and to ask a lot of questions. Be sure you pick the school that is the best match for your child.')}}
               </p>
-              <h1>{{ __('Governance and Operations')}}</h1>
-              <p class="lead">
-              {{ __('We want to practice servant leadership: teachers serve students; administrators serve teachers.
-              We want to make what is best for our students a top priority.
-              We want to emphasize the spirit of the law over the letter of the law.
-              We want to emphasize ‘who we are’ is more important than ‘what we do’.
-              We want to place overall, long-term benefits over expediency.')}}
-              </p>
-              <h1>{{ __('Methodology')}}</h1>
-              <p class="lead">
-              {{ __('We want to make sure the teacher is the key to the classroom.
-              We want to equip our teachers to teach: spiritual health, professional training, tools and resources, supportive and positive environment.
-              We want to work in partnership with parents and emphasize comprehensive development: academic, physical, psychological, social, cultural.')}}
-              </p>
+              <br>
+              <a class="btn btn-primary" href="\documents">Application .PDF</a>
           </div>
           <div class="col">
             @include('inc.sidebarlinks')
