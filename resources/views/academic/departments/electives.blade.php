e99ca4bda146c1103926bee76084266d35714958
Chad Quilter <chadquilter@gmail.com>
2018-04-10 13:16:28 -0500

    update page header

--- resources/views/academic/departments/electives.blade.php
+++ resources/views/academic/departments/electives.blade.php
@@ -6,7 +6,7 @@
     <p class="lead">{{ __('Electives are an opportunity for students to develop their skills and interests in particular fields. Students are graded primarily on participation and effort and are encouraged to think and live beyond themselves and to discover their life’s mission and goal.')}}</p>
     <hr class="style-two">
 
-    <input type="button"  class="btn btn-outline-primary btn-block subdept-button" value="{{__('List of Electives')}}">
+    <input type="button"  class="btn btn-outline-primary btn-block subdept-button" value="{{__('Elective Options')}}">
     <div class="card elecdepartmentDiv">
       <div class="card-header">
         <h1>{{ __('Elective Options:')}}</h1>
