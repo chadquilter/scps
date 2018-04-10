24ec03a206cbf670bf49a186b189a44590ceffb2
Chad Quilter <chadquilter@gmail.com>
2018-04-10 13:06:41 -0500

    update page header

--- resources/views/student/growth.blade.php
+++ resources/views/student/growth.blade.php
@@ -7,26 +7,10 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-              <h1>{{ __('Scope of our Mission')}}</h1>
+              <h1>{{ __('Student Life!')}}</h1>
+              <hr class="style-two">
               <p class="lead">
-              {{ __('We want to reach all who can benefit from our school.
-              We want to develop each student to their full potential.
-              We want to emphasize spiritual results over academic achievement.
-              We want to be faithful to our task and trust God to give the increase.')}}
-              </p>
-              <h1>{{ __('Governance and Operations')}}</h1>
-              <p class="lead">
-              {{ __('We want to practice servant leadership: teachers serve students; administrators serve teachers.
-              We want to make what is best for our students a top priority.
-              We want to emphasize the spirit of the law over the letter of the law.
-              We want to emphasize ‘who we are’ is more important than ‘what we do’.
-              We want to place overall, long-term benefits over expediency.')}}
-              </p>
-              <h1>{{ __('Methodology')}}</h1>
-              <p class="lead">
-              {{ __('We want to make sure the teacher is the key to the classroom.
-              We want to equip our teachers to teach: spiritual health, professional training, tools and resources, supportive and positive environment.
-              We want to work in partnership with parents and emphasize comprehensive development: academic, physical, psychological, social, cultural.')}}
+              {{ __('Middle school and high school clubs allow our students to find their passion outside of the classroom. A broad range of clubs and organizations allow each student to explore areas of interest, exercise leadership skills, and develop meaningful relationships with students who have similar interests. We encourage students to start new clubs, or step out of their comfort zones and join an established club they may not have considered in the past. At the beginning of each school year we hold a “Club Fair” where all of our clubs are on display, officers are manning tables, and literature is distributed to provide exposure to all of the groups. Our goal is that each student will find a place outside of the classroom to grow, create, and develop.')}}
               </p>
           </div>
           <div class="col">
