0ff93e2260e3cff425e8385a406eeee473cbea35
Chad Quilter <chadquilter@gmail.com>
2018-04-10 13:12:26 -0500

    update page header

--- resources/views/international/acceleratedenglish.blade.php
+++ resources/views/international/acceleratedenglish.blade.php
@@ -8,6 +8,7 @@
         <div class="row">
           <div class="col">
               <h1>{{ __('Accelerated English Camp')}}</h1>
+              <hr class="style-two">
               <p class="lead">
               {{ __('Accelerated English Camp is specifically designed to help students transition to an American high school on an academic, social, and personal level. The intensive four-week program will provide students with instruction on American culture and norms, strengthen their conversational English, and increase their academic vocabulary in order to prepare them for entering an American high school shortly thereafter.')}}
               </p>
--- resources/views/international/partners.blade.php
+++ resources/views/international/partners.blade.php
@@ -7,7 +7,8 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-              <h1>{{ __('Scope of our Mission')}}</h1>
+              <h1>{{ __('International Partners Welcome!')}}</h1>
+              <hr class="style-two">
               <p class="lead">
               {{ __('Space City Preparatory has a highly esteemed international reputation for developing programs that combine the best teaching resources and methods of the East and the West. SCPS staff regularly work with schools and education organizations overseas to provide expertise and experience in providing a transformative education that prepares students for the best universities in the world.')}}
               </p>
