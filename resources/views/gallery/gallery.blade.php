3e1cfac37fe100d37a87a471b8a2b0ea4fa5c159
Chad Quilter <chadquilter@gmail.com>
2018-04-11 17:08:47 -0500

    update web routes and nav links for more SEO categories

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -1,7 +1,7 @@
 <div class="gallery">
   <div class="card">
     <div class="card-header">
-      <h2 class="display-2">{{__($title)}}</h2>
+      <h4 class="display-4">{{__($title)}}</h4>
     </div>
     <div class="card-body alt-color">
       @include('inc.jobimagecarosel')
