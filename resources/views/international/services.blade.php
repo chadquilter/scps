27f6fb1db8c402c79118660713ae5ef8df2d6dbf
Chad Quilter <chadquilter@gmail.com>
2018-04-11 20:26:40 -0500

    update web routes and nav links for more SEO categories

--- resources/views/international/services.blade.php
+++ resources/views/international/services.blade.php
@@ -7,18 +7,18 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-              <h1>{{ __('International Services and Camps')}}</h1>
+              <h2><u>{{ __('International Services and Camps')}}</u></h2>
               <p class="lead">
               {{ __('Each year, we welcome international students to Space City Preparatory from China and other countries. These families value our rich academic program and entrust their children’s futures to our staff and school community. We take our role seriously, providing special services to help our international students as they make the transition to life in America.')}}
               </p>
-              <h1>{{ __('Transitional Services')}}</h1>
+              <h2><u>{{ __('Transitional Services')}}<u></h2>
               <p class="lead">
               {{ __('International students who are refining their English language skills have access to classes specially designed for those learning English as a second language. We offer sheltered classes for credit in some subjects for English language learners. We have a bilingual high school program in Mandarin and English called The Academy at SCPS, which provides intensive English instruction and academic support. In addition, a four-week summer program Accelerated English Camp, is designed to transition newly arrived students academically, socially, and culturally to American school life.')}}
               </p>
               <p class="lead">
                 {{__('Our International Student Services (ISS) group provides additional services, including cultural orientations, housing support, and counseling about healthcare and personal emergencies.')}}
               </p>
-              <h1>{{ __('Communication')}}</h1>
+              <h2><u>{{ __('Communication')}}</u></h2>
               <p class="lead">
               {{ __('We maintain strong ties with family members back home in their native language through regular newsletters, e-mail, Skype, and our online tool, Powerschool, which gives parents access to ongoing updates on their child’s progress. We have translators available for parents and students alike, and our written communications are provided in English and Chinese.')}}
               </p>
