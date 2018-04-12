569124688bf26a1d239f6708c02967464c1aeed8
Chad Quilter <chadquilter@gmail.com>
2018-04-11 20:16:26 -0500

    update web routes and nav links for more SEO categories

--- resources/views/admission/tuitionfees.blade.php
+++ resources/views/admission/tuitionfees.blade.php
@@ -38,19 +38,19 @@
                 <h4 class="display-4">{{ __('Participation Fees')}}</h4>
               </div>
               <div class="card-body">
-                <h1>{{ __('Class Trips:')}}</h1>
+                <h1><u>{{ __('Class Trips:')}}</u></h1>
                 <p class="lead">
                   {{ __('Students participate in educational trips during the school year. Details for each trip are provided early in the school year so students can plan and fundraise for these award-winning experiences.')}}
                 </p>
-                <h1>{{ __('Interscholastic Sports:')}}</h1>
+                <h1><u>{{ __('Interscholastic Sports:')}}</u></h1>
                 <p class="lead">
                   {{ __('Students selected to participate on an interscholastic sports team are required to pay a per season fee to defray some of the cost for participation, transportation, coach salaries, and league expenses. Students must submit a recent medical clearance and complete all required paperwork in advance. Please refer to the Sports Participation Manual for more details.')}}
                 </p>
-                <h1>{{ __('Special Activities:')}}</h1>
+                <h1><u>{{ __('Special Activities:')}}</u></h1>
                 <p class="lead">
                   {{ __('Participation in certain groups, activities, and competitions may require students to pay for travel (including overnight travel) to events, competitions, team uniforms or performance wardrobe, and other expenses. The cost for participation and the schedule of events is made available in advance and students and parents must commit to meeting their obligations before being allowed to join.')}}
                 </p>
-                <h1>{{ __('Other Services:')}}</h1>
+                <h1><u>{{ __('Other Services:')}}</u></h1>
                 <p class="lead">
                   {{ __('Space City Preparatory has a Food Service, Transportation Service, and After School Tutoring Service. There are also special classes and enrichment opportunities throughout the year. Please refer to our specific literature for more information about each program.')}}
                 </p>
@@ -63,19 +63,19 @@
                 <h4 class="display-4">{{ __('Financial Policies')}}</h4>
               </div>
               <div class="card-body">
-                <h1>{{ __('Refunds:')}}</h1>
+                <h1><u>{{ __('Refunds:')}}</u></h1>
                 <p class="lead">
                   {{ __('School fees (e.g. testing, registration, etc.) are non-refundable. Refunds on items not received or used are subject to the policies of the publisher, manufacturer, or supplier.')}}
                 </p>
-                <h1>{{ __('Cancellations:')}}</h1>
+                <h1><u>{{ __('Cancellations:')}}</u></h1>
                 <p class="lead">
                   {{ __('Cancellation of annual services (e.g. tuition, transportation, tutoring, etc.) is subject to a 10% charge based on the annual amount. After the first semester, tuition will not be pro-rated or refunded for the remainder of the year even if the student withdraws.')}}
                 </p>
-                <h1>{{ __('Schedule:')}}</h1>
+                <h1><u>{{ __('Schedule:')}}</u></h1>
                 <p class="lead">
                   {{ __('First installments are due June 15, 2018, then on the 1st of every month from September 1, 2018, through May 1, 2019. Additional monthly statements may be issued for your convenience.')}}
                 </p>
-                <h1>{{ __('Unpaid Balances:')}}</h1>
+                <h1><u>{{ __('Unpaid Balances:')}}</u></h1>
                 <p class="lead">
                   {{ __('Payments are considered overdue after the 5th of each month at which time a minimum late fee of $30/month or 1.5% of the outstanding balance will be charged. Families will be charged $30 for each occurrence of a returned check plus any bank charges incurred by the school. Families with severely delinquent accounts will not be allowed to re-enroll and may be refused attendance until the balance is settled. Please contact the school office before letting your account balance become a problem.')}}
                 </p>
