bf61b7b996c3ce66a84c42de704e7865ca1d96c2
Chad Quilter <chadquilter@gmail.com>
2018-04-11 20:31:00 -0500

    update web routes and nav links for more SEO categories

--- resources/views/about/schoolhistory.blade.php
+++ resources/views/about/schoolhistory.blade.php
@@ -8,14 +8,15 @@
         <div class="row">
           <div class="col">
               <h1>{{ __('Our Past:')}}</h1>
+              <hr class="style-two">
               <p class="lead">
               {{ __('We started as a small school in Houston, TX that emphisized quality over quantity teaching practices.')}}
               </p>
-              <h1>{{ __('Our Present:')}}</h1>
+              <h1><u>{{ __('Our Present:')}}</u></h1>
               <p class="lead">
               {{ __('Our school has grown to include a brand new campus with camps and elective programs in Austin as well as Houston, TX.')}}
               </p>
-              <h1>{{ __('Our Future:')}}</h1>
+              <h1><u>{{ __('Our Future:')}}</u></h1>
               <p class="lead">
               {{ __('We plan to keep the same quality and mentorship that made our legacy what it is today.')}}
               </p>
