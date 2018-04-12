9e7057a45d44178decb0d063fa102e3ee315e2f3
Chad Quilter <chadquilter@gmail.com>
2018-04-12 18:43:55 -0500

    fix index page hyperlinks

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -29,10 +29,12 @@
     </div>
     <div class="col col-sm-10">
       <div class="row">
+        <a href="/">
         <div class="col">
-          <a href="/"><h1 class="display-4" style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000;">{{__('Space City Preparatory School')}}</h1>
-          <h5 style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000; font-style: oblique;">{{__('~ Educating The World.')}}</h5></a>
-        </div>
+          <h1 class="display-4" style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000;">{{__('Space City Preparatory School')}}</h1>
+          <h5 style="text-align: left; color: #FAEBD7; text-shadow: .5rem .5rem 1rem #000000; font-style: oblique;">{{__('~ Educating The World.')}}</h5>
+        </div></a>
+
         <!-- Split dropup button -->
         <div class="col col-auto float-sm-right">
           <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
