58703a8eda0d7b56992af536a18902283706284a
Chad Quilter <chadquilter@gmail.com>
2018-04-12 23:09:30 -0500

    update mail controllers to include subject line

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -24,7 +24,7 @@ class ContactMail extends Mailable
     {
         $this->contact = $contact;
         //add subject line to contact
-        $subject = 'Hello, a contact has been sent from '.$this->contact->name.' to Space City.';
+        $subject = 'Hello, a contact has been sent from '.$this->contact->name.' to Space City Preparatory.';
         $this->subject($subject);
     }
 
