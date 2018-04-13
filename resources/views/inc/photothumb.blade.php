1ab0fae411fb42ce53d72485711aabac8cbd51f9
Chad Quilter <chadquilter@gmail.com>
2018-04-12 19:13:22 -0500

    update image class to fluid

--- resources/views/inc/photothumb.blade.php
+++ resources/views/inc/photothumb.blade.php
@@ -6,37 +6,40 @@
   </div>
 </div>
 <div class="row">
-  <div class="col">
+  <div class="col-sm">
         <div class="img-container">
           <a href="/gallery/art-gallery">
-            <img class="img-fluid img-thumbnail" src="/images/gallery_icons/Art-135x110.jpg" alt="Avatar" class="img-image">
+            <img class="img-fluid" src="/images/gallery_icons/Art-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
-
+  </div>
+  <div class="col-sm">
     <div class="img-container">
       <a href="/gallery/classroom-gallery">
-        <img class="img-fluid img-thumbnail" src="/images/gallery_icons/Classrooms-135x110.jpg" alt="Avatar" class="img-image">
+        <img class="img-fluid" src="/images/gallery_icons/Classrooms-135x110.jpg" alt="Avatar" class="img-image">
       </a>
     </div>
-
+  </div>
+  <div class="col-sm">
         <div class="img-container">
           <a href="/gallery/electives-gallery">
-            <img class="img-fluid img-thumbnail" src="/images/gallery_icons/Electives-135x110.jpg" alt="Avatar" class="img-image">
+            <img class="img-fluid" src="/images/gallery_icons/Electives-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
-
+  </div>
+  <div class="col-sm">
         <div class="img-container">
           <a href="/gallery/sports-gallery">
-            <img class="img-fluid img-thumbnail" src="/images/gallery_icons/Sports-135x110.jpg" alt="Avatar" class="img-image">
+            <img class="img-fluid" src="/images/gallery_icons/Sports-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
-
+  </div>
+  <div class="col-sm">
         <div class="img-container">
           <a href="/gallery/student-life-gallery">
-            <img class="img-fluid img-thumbnail" src="/images/gallery_icons/Studentlife-135x110.jpg" alt="Avatar" class="img-image">
+            <img class="img-fluid" src="/images/gallery_icons/Studentlife-135x110.jpg" alt="Avatar" class="img-image">
           </a>
         </div>
-
-    </div>
   </div>
+
 </div>
