5280550bc7dd089612dec8f80f1be3fa36aa8ab4
Chad Quilter <chadquilter@gmail.com>
2018-04-12 22:56:19 -0500

    update mail controllers to include subject line

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -24,9 +24,6 @@
           <strong>Email:</strong>
           <p class="lead">{{ $contactEmail }}</p>
         </div>
-        <div class="card-footer">
-          <small>This email was intended for {{ $contactName}}.</small>
-        </div>
       </div>
       <br>
       <div class="main-color text-light sidebar_pad shadow_only">
@@ -37,6 +34,9 @@
           <hr class="style-two">
           <p class="lead">{{ $contactNotes }}</p>
           <hr class="style-two">
+        </div>
+        <div class="card-footer">
+          <small>This email was intended for {{ $contactName}}.</small>
         </div>
       </div>
     </div>
