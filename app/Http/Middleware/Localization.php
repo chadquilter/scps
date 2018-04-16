d2bcdbfd6e2719fe1e4887e406c1080b002061c5
Chad Quilter <chadquilter@gmail.com>
2018-04-16 17:56:08 -0500

    add our language selector

---
+++ app/Http/Middleware/Localization.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Request;
+
+class Localization
+{
+
+    protected $app;
+
+    public function __construct(Application $app, Request $request)
+    {
+        if(Session::has('lang'))
+        {
+            $lang = Session::get('lang');
+            $app->setLocale($lang);
+        }
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
