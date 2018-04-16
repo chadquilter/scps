a0aa054bc64300c632c9bcd5bee178d851219610
Chad Quilter <chadquilter@gmail.com>
2018-04-16 18:03:25 -0500

    add our language selector

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\Localization::class, 
+        \App\Http\Middleware\Localization::class,
     ];
 
     /**
--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -5,17 +5,18 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class Localization
 {
 
     protected $app;
 
-    public function __construct(Application $app, Request $request)
+    public function __construct(Application $app, Request $request, Session $session)
     {
         if(Session::has('lang'))
         {
-            $lang = Session::get('lang');
+            $lang = $session->get('lang');
             $app->setLocale($lang);
         }
     }
