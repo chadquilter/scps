e8babdb9082aab36d5177910c70cdb5b9583c84e
Chad Quilter <chadquilter@gmail.com>
2018-04-16 17:03:27 -0500

    add our language selector

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 use Storage;
 
 class PagesController extends Controller
