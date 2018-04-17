8f2466c11009e7de58c6399a47d901fa5314963f
Chad Quilter <chadquilter@gmail.com>
2018-04-17 15:13:39 -0500

    add markup to fix outlook mail layout

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="CNQ Software Services">
@@ -11,6 +12,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{__('Space City Preparatory School') }}</title>
     <!-- Styles -->
+    <style type="text/css">
+      body {margin: 0; padding: 0; min-width: 100%!important;}
+      .content {width: 100%; max-width: 600px;}
+    </style>
 </head>
 <body style="background-color: #404040;">
   <main role="main">
