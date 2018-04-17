fc9161d919c6a86f97318183932c4959b5d68c3f
Chad Quilter <chadquilter@gmail.com>
2018-04-17 13:20:25 -0500

    add xlations to emal reciepts for multi-lingual emails

--- app/Mail/ReceiptMail.php
+++ app/Mail/ReceiptMail.php
@@ -25,7 +25,7 @@ class ReceiptMail extends Mailable
         $this->contact = $contact;
         $this->to($this->contact->email, $this->contact->name);
         $to = explode(',', env('ADMIN_EMAILS'));
-        $this->to($to, 'SCPS Administration');
+        $this->cc($to, 'SCPS Administration');
         $this->subject('Hello '.$this->contact->name.', thanks for contacting Space City Preparatory!');
     }
 
