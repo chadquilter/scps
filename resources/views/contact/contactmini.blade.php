c2a02301686d30abf860ff49a0c22296dcb2c480
Chad Quilter <chadquilter@hotmail.com>
2018-04-18 22:17:19 -0500

    add new sidebar inc files

--- resources/views/contact/contactmini.blade.php
+++ resources/views/contact/contactmini.blade.php
@@ -1,42 +1,39 @@
 
-  <div class="main-color">
-
-        @include('inc.messages')
-        <h4 class="shadow_text">{{ __('Contact Us!') }}</h4>
-        <hr>
-        <form id="contact_form_mini" action={{ url('/contact/store')  }} method="POST" enctype="multipart/form-data">
-          @csrf
-          <div class="form-group row">
-            <div class="col">
-              <label for="name" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Name:') }}</strong></label><br>
-              <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <div class="col">
-              <label for="email" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Email:') }}</strong></label><br>
-              <input id="text" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <div class="col">
-              <label for="phone" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Phone:') }}</strong></label><br>
-              <input id="phone" type="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <div class="col">
-              <label for="notes" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Message:') }}</strong></label><br>
-              <textarea id="notes" rows="7" cols="40" class="form-control{{ $errors->has('notes') ? ' is-invalid' : '' }}" name="notes" required autofocus>{{ old('notes') }}</textarea>
-            </div>
-          </div>
-            <div class="form=group">
-              <input id="submit_button" type="submit" value="{{ __('Send Email')}}" />
-            </div>
-          </form>
+<div class="card">
+  <div class="card-body main-color">
+    @include('inc.messages')
+    <h4 class="shadow_text">{{ __('Contact Us!') }}</h4>
+    <hr>
+    <form id="contact_form_mini" action={{ url('/contact/store')  }} method="POST" enctype="multipart/form-data">
+      @csrf
+      <div class="form-group row">
+        <div class="col">
+          <label for="name" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Name:') }}</strong></label><br>
+          <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
         </div>
-
-    </div>
+      </div>
+      <div class="form-group row">
+        <div class="col">
+          <label for="email" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Email:') }}</strong></label><br>
+          <input id="text" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col">
+          <label for="phone" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Phone:') }}</strong></label><br>
+          <input id="phone" type="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col">
+          <label for="notes" class="shadow_text col-sm-4 col-form-label text-md-right"><strong>{{ __('Message:') }}</strong></label><br>
+          <textarea id="notes" rows="7" cols="40" class="form-control{{ $errors->has('notes') ? ' is-invalid' : '' }}" name="notes" required autofocus>{{ old('notes') }}</textarea>
+        </div>
+      </div>
+      <div class="form=group">
+        <input id="submit_button" type="submit" value="{{ __('Send Email')}}" />
+      </div>
+    </form>
+  </div>
+</div>
+</div>
