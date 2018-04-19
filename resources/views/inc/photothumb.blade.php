1614db4061037ac7eff29ba10ae969c920acedff
Chad Quilter <chadquilter@gmail.com>
2018-04-19 14:01:10 -0500

    use flex-grid overlay to cover image text with app text

--- resources/views/inc/photothumb.blade.php
+++ resources/views/inc/photothumb.blade.php
@@ -6,21 +6,21 @@
   </div>
 </div>
 <div class="row">
-  <div class="col">
+  <div class="col-sm">
     <br>
     <div class="row">
-        <div class="col-md-4">
+        <div class="col">
             <div class="card bg-inverse">
-              <div class="img-container">
+
                 <img class="card-img" src="/images/gallery_icons/Art-135x110.jpg" alt="Art Gallery">
                 <div class="card-img-overlay h-100 d-flex flex-column justify-content-end alert alert-dark">
                     <h4 class="card-title">Art Gallery</h4>
                 </div>
-              </div>
+
             </div>
         </div>
-        <div class="col-md-4"></div>
-        <div class="col-md-4"></div>
+        <div class="col"></div>
+        <div class="col"></div>
     </div>
 
   </div>
