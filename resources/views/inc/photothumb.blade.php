c0754b7484ce393f9ea796a5205ec42fec4e4a65
Chad Quilter <chadquilter@gmail.com>
2018-04-19 17:56:22 -0500

    use flex-grid overlay to cover image text with app text

--- resources/views/inc/photothumb.blade.php
+++ resources/views/inc/photothumb.blade.php
@@ -11,7 +11,7 @@
   <div class="col-sm">
     <br>
     <a href="{{ $link["link"] }}" class="nounderline">
-    <div class="card card-inverse text-center img-container shadow_only" style="min-width: 8rem; max-width: 8rem;">
+    <div class="card card-inverse text-center img-container shadow_only" style="min-width: 10rem; max-width: 10rem;">
       <!-- Image -->
       <img class="card-img-top img-image" src="{{ $link["image_link"] }}" alt="Thumbnail to {{ $link["label"] }}">
       <!-- Text Overlay -->
