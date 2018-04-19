66e20a67bb5e2d4d24b101f45bd14a7ad3853f89
Chad Quilter <chadquilter@hotmail.com>
2018-04-19 16:59:13 -0500

    test overlay text for gallery thumbnails

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -12,6 +12,19 @@ class GalleryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    private function gallery_items(){
+         $links = array(
+           'art' => array('link' => '/gallery/art-gallery', 'image_link' => '/images/gallery_icons/Art-135x110.jpg', 'label' => 'Art'),
+           'classrooms' => array('link' => '/gallery/classroom-gallery', 'image_link' => '/images/gallery_icons/Classrooms-135x110.jpg', 'label' => 'Classroom'),
+           'electives' => array('link' => '/gallery/electives-gallery', 'image_link' =>  '/images/gallery_icons/Electives-135x110.jpg', 'label' => 'Electives'),
+           'sports' => array('link' => '/gallery/sports-gallery', 'image_link' =>  '/images/gallery_icons/Sports-135x110.jpg', 'label' => 'Sports'),
+           'studentlife' => array('link' => '/gallery/student-life-gallery', 'image_link' =>  '/images/gallery_icons/Studentlife-135x110.jpg', 'label' => 'Life'),
+         );
+         return $links;
+    }
+
+
      private function sidelinks(){
        $links = array(
         'athletics' => array('link' => 'athletics', 'label' => 'Student Athletics'),
@@ -24,9 +37,11 @@ class GalleryController extends Controller
       $title = 'Sports Photo Gallery';
       $sidelinks = $this->sidelinks();
       $files = Storage::disk('images')->files('gallery/sports');
+      $links = $this->gallery_items();
       return view('gallery.sports-gallery')
         ->with('sidelinks', $sidelinks)
         ->with('files', $files)
+        ->with('links'. $links)
         ->with('title', $title);
     }
 
@@ -34,9 +49,11 @@ class GalleryController extends Controller
       $title = 'Student Life Photo Gallery';
       $sidelinks = $this->sidelinks();
       $files = Storage::disk('images')->files('gallery/student');
+      $links = $this->gallery_items();
       return view('gallery.student-life-gallery')
         ->with('sidelinks', $sidelinks)
         ->with('files', $files)
+        ->with('links', $links)
         ->with('title', $title);
     }
 
@@ -44,9 +61,11 @@ class GalleryController extends Controller
       $title = 'Art Photo Gallery';
       $sidelinks = $this->sidelinks();
       $files = Storage::disk('images')->files('gallery/art');
+      $links = $this->gallery_items();
       return view('gallery.art-gallery')
         ->with('sidelinks', $sidelinks)
         ->with('files', $files)
+        ->wtih('links', $links)
         ->with('title', $title);
     }
 
@@ -54,9 +73,11 @@ class GalleryController extends Controller
       $title = 'Elective Photo Gallery';
       $sidelinks = $this->sidelinks();
       $files = Storage::disk('images')->files('gallery/elective');
+      $links = $this->gallery_items();
       return view('gallery.elective-gallery')
         ->with('sidelinks', $sidelinks)
         ->with('files', $files)
+        ->with('links', $links)
         ->with('title', $title);
     }
 
@@ -64,9 +85,11 @@ class GalleryController extends Controller
       $title = 'Classroom Photo Gallery';
       $sidelinks = $this->sidelinks();
       $files = Storage::disk('images')->files('gallery/classroom');
+      $links = $this->gallery_items();
       return view('gallery.classroom-gallery')
         ->with('sidelinks', $sidelinks)
         ->with('files', $files)
+        ->with('links', $links)
         ->with('title', $title);
     }
 
