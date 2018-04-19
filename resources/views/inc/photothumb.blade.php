b51768c3dc7fa52be4fcd9af283a6d5219b3f9e2
Chad Quilter <chadquilter@hotmail.com>
2018-04-19 15:59:44 -0500

    test overlay text for gallery thumbnails

--- resources/views/inc/photothumb.blade.php
+++ resources/views/inc/photothumb.blade.php
@@ -9,6 +9,28 @@
 
   <div class="col-sm">
     <br>
+
+
+<div class="card card-inverse text-center" style="max-width: 202px;">
+<!-- Image -->
+  <img class="card-img-top" src="/images/gallery_icons/Art-135x110.jpg" alt="Thumbnail to Art Gallery Section">
+  <!-- Text Overlay -->
+    <div class="card-img-overlay">
+      <h4 class="card-title">What Next?</h4>
+      <div class="alert alert-info">
+        <p class="card-text">Is this the end?</p>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+
+  </div>
+
+  <div class="col-sm">
+    <br>
         <div class="img-container">
           <a href="/gallery/art-gallery">
             <img class="img-image" src="/images/gallery_icons/Art-135x110.jpg" alt="Avatar" class="img-image">
