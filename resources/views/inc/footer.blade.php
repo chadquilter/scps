99830219d279220f07a2182cadd30e130ad706b9
Chad Quilter <chadquilter@hotmail.com>
2018-04-20 16:07:53 -0500

    test overlay text for gallery thumbnails

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -20,7 +20,7 @@
               Phone: (832) 205-2160
             </address>
           </div>
-          <div class="col">
+          <div class="col col-2">
             <p class="shadow_text">
               {{ __('Quick Links') }}
             </p>
@@ -37,10 +37,8 @@
               </li>
             </ul>
           </div>
-          <div class="col">
-            @include('contact.contactmini')
-          </div>
-          <div class="col">
+
+          <div class="col col-2">
             <div id="fb-root"></div>
             <script>(
               function(d, s, id) {
@@ -62,6 +60,11 @@
           </script>
             <div class="fb-page" data-href="https://www.facebook.com/spacecityschool" data-tabs="timeline" data-width="250" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/spacecityschool" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/spacecityschool">Space City Preparatory School</a></blockquote></div>
           </div>
+
+          <div class="col">
+            @include('contact.contactmini')
+          </div>
+
 
         </div>
     </div>
