b9cf5602a758beac4576d10a80a553135f278698
Chad Quilter <chadquilter@hotmail.com>
2018-04-20 15:31:53 -0500

    test overlay text for gallery thumbnails

--- resources/views/inc/jobimagecarosel.blade.php
+++ resources/views/inc/jobimagecarosel.blade.php
@@ -7,7 +7,7 @@
       <ol class="carousel-indicators">
         @foreach($files as $file)
           @php $active = $loop->index === 0 ? 'active' : ''; @endphp
-          <li data-target="#carouselExampleIndicators" data-slide-to="{{$loop->index}}" class={{$active}}></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="{{$loop->index}}" class="{{$active}}"></li>
         @endforeach
       </ol>
       <div class="carousel-inner embed-responsive embed-responsive-16by9" role="listbox">
