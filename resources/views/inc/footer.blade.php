352b5cb2f04ecf7dc0363b625a83f22f15eee1d4
Chad Quilter <chadquilter@hotmail.com>
2018-04-20 16:06:34 -0500

    test overlay text for gallery thumbnails

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="container">
 <footer class="footer main-color text-white">
-
-      <div class="row">
-
+  <div class="foot-container">
+    <div class="footer-text">
+        <div class="row">
           <div class="col">
             <p class="shadow_text">
               {{_('2018, '.config('app.name', 'SCPS'))}}
@@ -12,7 +12,7 @@
               {{_('Private Middle & High School')}}
               Houston, TX | Pasadena, TX | Marblefalls, TX
             </p>
-            <hr class="style-two">
+            <hr class="style-two1">
             <address class="shadow_text">
               P.O. Box 590743 <br>
               Houston, TX<br>
@@ -20,7 +20,6 @@
               Phone: (832) 205-2160
             </address>
           </div>
-
           <div class="col">
             <p class="shadow_text">
               {{ __('Quick Links') }}
@@ -38,14 +37,34 @@
               </li>
             </ul>
           </div>
-
           <div class="col">
             @include('contact.contactmini')
           </div>
+          <div class="col">
+            <div id="fb-root"></div>
+            <script>(
+              function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
 
+            $(function() {
+              $(document).scrollTop();
+            });
 
+            $(document).ready(function(){
+               $(window).scrollTop(0);
+           });
 
-      </div>
+          </script>
+            <div class="fb-page" data-href="https://www.facebook.com/spacecityschool" data-tabs="timeline" data-width="250" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/spacecityschool" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/spacecityschool">Space City Preparatory School</a></blockquote></div>
+          </div>
 
+        </div>
+    </div>
+  </div>
 </footer>
 </div>
