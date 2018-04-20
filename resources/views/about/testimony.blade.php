00449504905fa83b30b41cd50dbd3e5f9b137faa
Chad Quilter <chadquilter@hotmail.com>
2018-04-20 00:24:51 -0500

    test overlay text for gallery thumbnails

--- resources/views/about/testimony.blade.php
+++ resources/views/about/testimony.blade.php
@@ -7,17 +7,26 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
+              <blockquote class="blockquote">
               <h3 class="display-3">“</h3>
               {{ __('...unlike most high schools, SCPS was like a home. Not only because of the circumstances that caused me to see the place as my home physically, but more because of the people, the teachers, the students, even the administration, that made me feel welcomed and loved, a feeling that can only be so real in a family.')}}
+              <footer class="blockquote-footer">Space City Student</footer>
+              </blockquote>
               <hr class="style-two">
+              <blockquote class="blockquote text-center">
               <h3 class="display-3">“</h3>
               {{ __('I am grateful for all the support and love that I had during my time there. A few of my favorite teachers and influential people in my life are still at Space City Preparatory! I hope that they continue teaching and influencing the next generation,')}}
+              <footer class="blockquote-footer">Space City Student</footer>
+              </blockquote>
               <hr class="style-two">
+              <blockquote class="blockquote text-right">
               <h3 class="display-3">“</h3>
               {{ __('the teachers, the students, even the administration, that made me feel welcomed and loved, a feeling that can only be so real in a family.')}}
+              <footer class="blockquote-footer">Space City Student</footer>
+              </blockquote>
               <hr class="style-two">
           </div>
-          <div class="co col-md-6">
+          <div class="co col-md-6 col-lg-4">
             @include('inc.sidebarlinks')
           </div>
         </div>
