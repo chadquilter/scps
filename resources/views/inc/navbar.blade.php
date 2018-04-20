06f73efd846efbd53a960261727767b95456b58e
Chad Quilter <chadquilter@gmail.com>
2018-04-19 23:19:24 -0500

    use flex-grid overlay to cover image text with app text

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -45,9 +45,9 @@
           </div>
           <form id="contact_form row" action={{ url('/search-scps')  }} method="POST" enctype="multipart/form-data">
             @csrf
-            <div class="input-group input-group-sm">
-              <div class="col input-group-prepend">
-                <input type="text" class="form-control" size="10" placeholder="{{ __('Search')}}" aria-label="{{ __('Search')}}" aria-describedby="basic-addon1">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <input type="text" class="form-control form-control-sm" size="15" placeholder="{{ __('Search')}}" aria-label="{{ __('Search')}}" aria-describedby="basic-addon1">
               </div>
               <button class="btn main-color border border-dark btn-sm text-light" type="button" style="">{{ __('Search')}}</button>
             </div>
