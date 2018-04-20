2f1932b8986f08bfcacae7791d56dc1822bf6aec
Chad Quilter <chadquilter@hotmail.com>
2018-04-20 15:28:31 -0500

    test overlay text for gallery thumbnails

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -57,8 +57,13 @@
 
           </div>
           <br>
-          <div class="col col-lg-4">
-
+          <div class="col col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xlg-4">
+            @include('inc.sidenewstudent')
+            <br>
+            @include('inc.sidealert')
+            <br>
+            @include('inc.sidecalendar')
+            <br>
           </div>
         </div>
         @include('inc.photothumb')
