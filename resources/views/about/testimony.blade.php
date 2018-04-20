fb14cf780ef671eaf5a37c2f421604a455a8cb15
Chad Quilter <chadquilter@hotmail.com>
2018-04-20 00:31:18 -0500

    test overlay text for gallery thumbnails

--- resources/views/about/testimony.blade.php
+++ resources/views/about/testimony.blade.php
@@ -7,13 +7,13 @@
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
-              <blockquote class="blockquote">
+              <blockquote class="blockquote text-right">
               <strong>“</strong>
               {{ __('...unlike most high schools, SCPS was like a home. Not only because of the circumstances that caused me to see the place as my home physically, but more because of the people, the teachers, the students, even the administration, that made me feel welcomed and loved, a feeling that can only be so real in a family.')}}
               <footer class="blockquote-footer">Space City Student</footer>
               </blockquote>
               <hr class="style-two">
-              <blockquote class="blockquote text-center">
+              <blockquote class="blockquote text-right">
               <strong>“</strong>
               {{ __('I am grateful for all the support and love that I had during my time there. A few of my favorite teachers and influential people in my life are still at Space City Preparatory! I hope that they continue teaching and influencing the next generation,')}}
               <footer class="blockquote-footer">Space City Student</footer>
