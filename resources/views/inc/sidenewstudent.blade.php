4a3c9074fde2c9870fcd11ca6290d38047af59ac
Chad Quilter <chadquilter@gmail.com>
2018-04-19 23:27:12 -0500

    use flex-grid overlay to cover image text with app text

--- resources/views/inc/sidenewstudent.blade.php
+++ resources/views/inc/sidenewstudent.blade.php
@@ -3,7 +3,7 @@
       <div class="main-color text-light sidebar_pad">
         <h1>
           <div class="row">
-            <div class="col col-sm-2">
+            <div class="col col-sm-2 col-xs-2">
               <img src="/images/new-course.png" border="0">
             </div>
             <div class="col">
