94149b178384c4dab6832e95e4e1889a12095d69
Chad Quilter <chadquilter@hotmail.com>
2018-04-20 19:48:16 -0500

    commit first tag

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -52,15 +52,12 @@
                 js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
                 fjs.parentNode.insertBefore(js, fjs);
               }(document, 'script', 'facebook-jssdk'));
-
-              $(function() {
-                $(document).scrollTop();
-              });
-
-              $(document).ready(function(){
-                $(window).scrollTop(0);
-              });
-
+              </script>
+              <script>
+                $(window).load(function() {
+                  // chat face un focus
+                  $(window).scrollTop(0);
+                });
               </script>
               <div class="fb-page" data-href="https://www.facebook.com/spacecityschool" data-tabs="timeline" data-width="250" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/spacecityschool" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/spacecityschool">Space City Preparatory School</a></blockquote></div>
             </div>
