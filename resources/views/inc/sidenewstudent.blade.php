772cb8bbff532ad12d53ebd25d3869256de9b3f1
Chad Quilter <chadquilter@gmail.com>
2018-04-24 16:21:13 -0500

    update tution header

--- resources/views/inc/sidenewstudent.blade.php
+++ resources/views/inc/sidenewstudent.blade.php
@@ -1,32 +1,33 @@
-  <div class="row">
-    <div class="col">
-      <div class="main-color text-light sidebar_pad shadow_only">
-          <div class="row">
-            <div class="col col-2">
-              <img src="/images/new-course.png" border="0">
-            </div>
-            <div class="col text-left">
-              <h6>{{ __('New Students')}}</h6>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <h1>{{ __('Application')}}</h1>
-            </div>
-          </div>
+<div class="row">
+  <div class="col">
+    <div class="main-color text-light sidebar_pad shadow_only">
+      <div class="row">
+        <div class="col col-2">
+          <img src="/images/new-course.png">
+        </div>
+        <div class="col text-left">
+          <h6>{{ __('New Students')}}</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h1>{{ __('Application')}}</h1>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col">
-        <div class="card">
-          <div class="card-body image_display_r">
-            <div class="">
-              <h5><strong>{{__('Download your Application to enroll today!')}}</strong></h5>
-            </div>
-            <hr>
-            <button class="btn main-color border border-dark btn-sm text-light" type="button">{{__('New Students Application')}}</button>
-          </div>
-        </div>
-      </div>
   </div>
+</div>
+
+<div class="row">
+  <div class="col">
+    <div class="card">
+      <div class="card-body image_display_r">
+        <div class="">
+          <h5><strong>{{__('Download your Application to enroll today!')}}</strong></h5>
+        </div>
+        <hr>
+        <button class="btn main-color border border-dark btn-sm text-light" type="button">{{__('New Students Application')}}</button>
+      </div>
+    </div>
+  </div>
+</div>
