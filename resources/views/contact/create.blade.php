9a117730ece25b2941ae4d92e8a6aeaa1d24b1b9
Chad Quilter <chadquilter@gmail.com>
2018-04-24 17:51:26 -0500

    update tution header

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -27,28 +27,28 @@
 
 
                   <div class="form-group row">
-                    <label for="name" class="col-sm-4 col-form-label text-md-right"><strong>{{ __('Name:') }}</strong></label>
+                    <label for="name" class="col-form-label text-md-right"><strong>{{ __('Name:') }}</strong></label>
                     <div class="col-md-6">
                       <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
                     </div>
                   </div>
 
                   <div class="form-group row">
-                    <label for="email" class="col-sm-4 col-form-label text-md-right"><strong>{{ __('Email:') }}</strong></label>
+                    <label for="email" class="col-form-label text-md-right"><strong>{{ __('Email:') }}</strong></label>
                     <div class="col-md-6">
                       <input id="text" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
                     </div>
                   </div>
 
                   <div class="form-group row">
-                    <label for="phone" class="col-sm-4 col-form-label text-md-right"><strong>{{ __('Phone:') }}</strong></label>
+                    <label for="phone" class="col-form-label text-md-right"><strong>{{ __('Phone:') }}</strong></label>
                     <div class="col-md-6">
                       <input id="phone" type="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
                     </div>
                   </div>
 
                   <div class="form-group row">
-                    <label for="notes" class="col-sm-4 col-form-label text-md-right"><strong>{{ __('Message:') }}</strong></label>
+                    <label for="notes" class="col-form-label text-md-right"><strong>{{ __('Message:') }}</strong></label>
                     <div class="col-md-6">
                       <textarea id="notes" rows="7" cols="40" class="form-control{{ $errors->has('notes') ? ' is-invalid' : '' }}" name="notes" required autofocus>{{ old('notes') }}</textarea>
                     </div>
@@ -70,7 +70,7 @@
             </div>
             <br>
           </div>
-          <div class="col col-6">
+          <div class="col">
             @include('inc.sidebarcontacts')
           </div>
         </div>
