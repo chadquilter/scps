d96c81d8b66af13cfe328a05c132f1532bd8ae30
Chad Quilter <chadquilter@gmail.com>
2018-04-24 16:52:27 -0500

    update tution header

--- resources/views/inc/sidebarcontacts.blade.php
+++ resources/views/inc/sidebarcontacts.blade.php
@@ -8,39 +8,43 @@
 
 <div class="row shadow_only">
   <div class="col">
-<div class="card">
-  <div class="card-body">
-    <div class="mapouter">
-      <div class="gmap_canvas">
-        <iframe width="200" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=3519 burke rd,pasadena,TX,78504=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    <div class="card">
+      <div class="card-body">
+        <div class="mapouter">
+          <div class="gmap_canvas">
+            <iframe width="200" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=3519 burke rd,pasadena,TX,78504=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          <style>.mapouter{overflow:hidden;height:200px;width:200px;}.gmap_canvas {background:none!important;height:200px;width:200px;}</style>
+        </div>
       </div>
-      <style>.mapouter{overflow:hidden;height:200px;width:200px;}.gmap_canvas {background:none!important;height:200px;width:200px;}</style>
     </div>
   </div>
 </div>
-<br>
-<div class="main-color text-light sidebar_pad">
-  <h1>{{__('Contact Information')}}</h1>
-</div>
-<div class="card">
-  <div class="card-body">
-    <p>
-      {{__('Space City Preparatory School')}}
-      <br>{{__('P.O. Box 590743 ')}}
-      <br>{{__('Houston, TX')}}
-      <br>{{__('77259 U.S.')}}
-      <br>{{__('Phone Number: (832) 205-2160')}}
-    </p>
-    <hr class="lead">
-      <p><strong>{{__('Admissions:')}}</strong>
-        {{__('If you have questions about admissions, visit our admissions page or contact our Admissions Director at (832) 205-2160')}}
-      </p>
-      <p><strong>{{__('General Informations:')}}</strong><a href="mailto:general-info@spacecityschool.com">general-info@spacecityschool.com</a></p>
-      <p><strong>{{__('Admissions:')}}</strong><a href="mailto:admissions@spacecityschool.com">admissions@spacecityschool.com</a></p>
-      <p><strong>{{__('Job Opportunities:')}}</strong><a href="mailto:jobs@spacecityschool.com">jobs@spacecityschool.com</a></p>
-      <p><strong>{{__('Media & Marketing:')}}</strong><a href="mailto:media-marketing@spacecityschool.com">media-marketing@spacecityschool.com</a></p>
-    <hr class="lead">
+
+<div class="row shadow_only">
+  <div class="col">
+    <div class="main-color text-light sidebar_pad">
+      <h1>{{__('Contact Information')}}</h1>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <p>
+          {{__('Space City Preparatory School')}}
+          <br>{{__('P.O. Box 590743 ')}}
+          <br>{{__('Houston, TX')}}
+          <br>{{__('77259 U.S.')}}
+          <br>{{__('Phone Number: (832) 205-2160')}}
+        </p>
+        <hr class="lead">
+        <p><strong>{{__('Admissions:')}}</strong>
+          {{__('If you have questions about admissions, visit our admissions page or contact our Admissions Director at (832) 205-2160')}}
+        </p>
+        <p><strong>{{__('General Informations:')}}</strong><a href="mailto:general-info@spacecityschool.com">general-info@spacecityschool.com</a></p>
+        <p><strong>{{__('Admissions:')}}</strong><a href="mailto:admissions@spacecityschool.com">admissions@spacecityschool.com</a></p>
+        <p><strong>{{__('Job Opportunities:')}}</strong><a href="mailto:jobs@spacecityschool.com">jobs@spacecityschool.com</a></p>
+        <p><strong>{{__('Media & Marketing:')}}</strong><a href="mailto:media-marketing@spacecityschool.com">media-marketing@spacecityschool.com</a></p>
+        <hr class="lead">
+      </div>
+    </div>
   </div>
-</div>
-</div>
 </div>
