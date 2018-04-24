ca1805ef3ff105518181bba54b862449ac0a27e4
Chad Quilter <chadquilter@gmail.com>
2018-04-24 15:27:20 -0500

    update tution header

--- resources/views/inc/sidebarlinks.blade.php
+++ resources/views/inc/sidebarlinks.blade.php
@@ -3,7 +3,7 @@
   <div class="card-img-top main-color text-light sidebar_pad">
     <h1>{{__('Bookmarks')}}</h1>
   </div>
-  <div class="card-body shadow_only">
+  <div class="card-body main-color shadow_only">
     <ul class="list-group">
       @foreach($sidelinks as $sidelink)
         <li class="list-group-item">
