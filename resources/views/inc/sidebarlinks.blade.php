bd7edae8c219286e65028c4ebcb52ae996359ab9
Chad Quilter <chadquilter@hotmail.com>
2018-04-24 01:06:14 -0500

    figure out what is breaking the header...

--- resources/views/inc/sidebarlinks.blade.php
+++ resources/views/inc/sidebarlinks.blade.php
@@ -1,14 +1,16 @@
 
 <br>
 <div class="main-color text-light sidebar_pad shadow_only">
-  <h1>{{__('Bookmarks')}}</h1>
+  <h4>{{__('Bookmarks')}}</h4>
 </div>
 <div class="card">
   <div class="card-body shadow_only">
     <ul>
       @foreach($sidelinks as $sidelink)
-          <a class="btn btn-outline-danger btn-block {{ ( \Request::url() == url($sidelink["link"]) ) ? 'active' : '' }}" href="{{ url($sidelink["link"]) }}">{{ __($sidelink["label"])}}</a>
-          <br>
+          <li>
+            <a class="btn btn-outline-danger btn-block {{ ( \Request::url() == url($sidelink["link"]) ) ? 'active' : '' }}" href="{{ url($sidelink["link"]) }}">{{ __($sidelink["label"])}}</a>
+            <br>
+          </li>
       @endforeach
     </ul>
     <hr class="style-two">
