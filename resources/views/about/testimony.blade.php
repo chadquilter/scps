36a439d72b0a3c72964aa3769cac99408a331e3f
Chad Quilter <chadquilter@hotmail.com>
2018-04-23 23:08:35 -0500

    add more translations...my arm is killing me

--- resources/views/about/testimony.blade.php
+++ resources/views/about/testimony.blade.php
@@ -11,19 +11,19 @@
               <blockquote class="blockquote text-right">
               <strong>“</strong>
               {{ __('...unlike most high schools, SCPS was like a home. Not only because of the circumstances that caused me to see the place as my home physically, but more because of the people, the teachers, the students, even the administration, that made me feel welcomed and loved, a feeling that can only be so real in a family.')}}
-              <footer class="blockquote-footer">Space City Student</footer>
+              <footer class="blockquote-footer">{{ __('Space City Student')}}</footer>
               <hr class="style-two">
               </blockquote>
               <blockquote class="blockquote text-right">
               <strong>“</strong>
               {{ __('I am grateful for all the support and love that I had during my time there. A few of my favorite teachers and influential people in my life are still at Space City Preparatory! I hope that they continue teaching and influencing the next generation,')}}
-              <footer class="blockquote-footer">Space City Student</footer>
+              <footer class="blockquote-footer">{{ __('Space City Student')}}</footer>
               <hr class="style-two">
               </blockquote>
               <blockquote class="blockquote text-right">
               <strong>“</strong>
               {{ __('the teachers, the students, even the administration, that made me feel welcomed and loved, a feeling that can only be so real in a family.')}}
-              <footer class="blockquote-footer">Space City Student</footer>
+              <footer class="blockquote-footer">{{ __('Space City Student')}}</footer>
               <hr class="style-two">
               </blockquote>
           </div>
