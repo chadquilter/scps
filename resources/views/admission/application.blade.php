1632dffd5d6e13a4b8e932da10413c01c7d79188
Chad Quilter <chadquilter@hotmail.com>
2018-04-23 22:40:18 -0500

    add more translations...my arm is killing me

--- resources/views/admission/application.blade.php
+++ resources/views/admission/application.blade.php
@@ -9,7 +9,7 @@
           <div class="col">
               <h1>{{ __('Middle School & High School Registration:')}}</h1>
               <p class="lead">
-              {{ __('Thank you for your interest in Space City Preparatory. Your child’s education and training are very important to us and we want to assist you in making the right choice. Every school is different and not every learning environment is best suited for every child. California Crosspoint Academy offers an excellent and comprehensive college – preparatory academic program in a highly structured but supportive learning community. We encourage you to visit many schools and to ask a lot of questions. Be sure you pick the school that is the best match for your child.')}}
+              {{ __('Thank you for your interest in Space City Preparatory. Your child’s education and training are very important to us and we want to assist you in making the right choice. Every school is different and not every learning environment is best suited for every child. Space City Preparatory offers an excellent and comprehensive college – preparatory academic program in a highly structured but supportive learning community. We encourage you to visit many schools and to ask a lot of questions. Be sure you pick the school that is the best match for your child.')}}
               </p>
               <br>
               <a class="btn btn-primary" href="\documents">Application .PDF</a>
