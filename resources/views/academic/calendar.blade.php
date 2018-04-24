d7bcb89d3a60b2a0abaf16b2b37b88cdaf5fc83a
Chad Quilter <chadquilter@gmail.com>
2018-04-24 14:10:44 -0500

    add container

--- resources/views/academic/calendar.blade.php
+++ resources/views/academic/calendar.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="sub-container">
     <div class="card">
-      <div class="card-header"><h1>{{ __($title) }}</h1></div>
+      <div class="card-header"><h1><strong>{{ __($title) }}</strong></h1></div>
       <div class="card-body alt-color">
         <div class="row">
           <div class="col">
