a89da23c67b3fdc93b50291b780c41923b12487f
Chad Quilter <chadquilter@gmail.com>
2018-04-24 19:34:56 -0500

    update tution header

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="CNQ Software Services">
-    <meta name="keywords" content="{{__('meta_keywords')}}"/>
-    <meta name="description" content="{{__('page_description')}}"/>
+    <meta name="keywords" content="{{__('School, Learning in the US, Learn English In the US, Enlgish in the USA, Learning world wide, learn english, speak english, learn american culture, speak native language usa')}}"/>
+    <meta name="description" content="{{__('School, Learning in the US, Learn English In the US, Enlgish in the USA, Learning world wide, learn english, speak english, learn american culture, speak native language usa')}}"/>
     <!-- <meta name="robots" content="index, follow"> -->
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
