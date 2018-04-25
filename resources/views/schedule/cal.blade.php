f690940a884f7a13153229bca556c9d8947bf3ea
Chad Quilter <chadquilter@hotmail.com>
2018-04-25 01:27:47 -0500

    add beginings of calendar, and some cool js to come

---
+++ resources/views/schedule/cal.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.front')
+
+@section('content')
+
+
+<script>
+$(function() {
+
+  // page is now ready, initialize the calendar...
+
+  $('#calendar').fullCalendar({
+    // put your options and callbacks here
+  })
+
+});
+
+</script>
+
+<div class="sub-container">
+  <div class="jumbotron">
+    <div id='calendar'></div>
+  </div>
+</div>
