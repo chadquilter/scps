d42e439a3688ab03a8850c8c72afc248b3344460
Chad Quilter <chadquilter@hotmail.com>
2018-04-25 00:29:40 -0500

    sidelink madness, update scss

--- resources/views/inc/photothumb.blade.php
+++ resources/views/inc/photothumb.blade.php
@@ -10,7 +10,7 @@
     <div class="card-deck">
       @foreach($links as $link)
 
-        <div class="card card-inverse text-center img-container shadow_only border-warning grow" style="min-width: 8.0rem;">
+        <div class="card card-inverse text-center img-container shadow_only border-warning grow" style="min-width: 6.0rem; max-width: 9.5rem;">
           <!-- Image -->
           <a href="{{ $link["link"] }}" class="nounderline">
             <img class="card-img-top img-image bg-warning" src="{{ $link["image_link"] }}" alt="Thumbnail to {{ $link["label"] }}">
