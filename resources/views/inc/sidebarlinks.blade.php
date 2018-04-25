c8abefe489934d13caa5d796d743deed18ac9e2d
Chad Quilter <chadquilter@gmail.com>
2018-04-24 20:44:49 -0500

    add new css animations

--- resources/views/inc/sidebarlinks.blade.php
+++ resources/views/inc/sidebarlinks.blade.php
@@ -8,8 +8,8 @@
     <ul class="list-group">
       @foreach($sidelinks as $sidelink)
         <li class="list-group-item">
-          <div class="button-cool text-light">
-            <a class="btn btn-light text-light {{ ( \Request::url() == url($sidelink["link"]) ) ? 'active' : '' }}" href="{{ url($sidelink["link"]) }}">
+          <div class="button-cool text-light nounderline">
+            <a class=" {{ ( \Request::url() == url($sidelink["link"]) ) ? 'active' : '' }}" href="{{ url($sidelink["link"]) }}">
               {{ __($sidelink["label"])}}
               <span>{{ __($sidelink["label"])}}</span>
             </a>
