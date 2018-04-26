228476e488ed21c0870e1da7dd222c7ff0f1b625
Chad Quilter <chadquilter@hotmail.com>
2018-04-26 00:09:29 -0500

    begin backend work, update login page

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -3,10 +3,10 @@
 @section('content')
   <div class="sub-container">
     <div class="card">
+      <div class="card-header">
+        <h4 class="display-4">Dashboard </h4>
+      </div>
       <div class='card-body alt-color'>
-        <div class="card-header">
-          <h4 class="display-4">Dashboard </h4>
-        </div>
         <p class="lead"><strong> Welcome {{ Auth::user()->name }}</strong></p>
         <p class="lead"><strong> {{ $dt }} </strong></p>
         @include('inc.dashmenu')
