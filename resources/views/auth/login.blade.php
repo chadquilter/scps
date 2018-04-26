3eec47fec78ac707b175933e22a6e57f152427c5
Chad Quilter <chadquilter@hotmail.com>
2018-04-25 23:33:27 -0500

    begin backend work, update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.front')
 
 @section('content')
-<div class="sub-container">
+<div class="sub-container alt-color">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
