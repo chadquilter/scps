89e1f84b79e33595e199e0b3d44952f145117a88
Chad Quilter <chadquilter@hotmail.com>
2018-04-27 05:05:55 -0500

    add file upload vue component

--- resources/assets/js/components/FileUploadComponent.vue
+++ resources/assets/js/components/FileUploadComponent.vue
@@ -1,13 +1,23 @@
-//FileUploadComponent.vue
+// ImageuploadComponent.vue
 
 <template>
-    <div class="container" id="app">
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
                     <div class="card-header">File Upload Component</div>
                     <div class="card-body">
-                        We need to write here file upload HTML code.
+                       <div class="row">
+                          <div class="col-md-3" v-if="image">
+                              <img :src="image" class="img-responsive" height="70" width="90">
+                           </div>
+                          <div class="col-md-6">
+                              <input type="file" v-on:change="onImageChange" class="form-control">
+                          </div>
+                          <div class="col-md-3">
+                             <button class="btn btn-success btn-block" @click="uploadImage">Upload Image</button>
+                          </div>
+                       </div>
                     </div>
                 </div>
             </div>
@@ -17,6 +27,33 @@
 
 <script>
     export default {
-
+        data(){
+            return {
+                image: ''
+            }
+        },
+        methods: {
+            onImageChange(e) {
+                let files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                    return;
+                this.createImage(files[0]);
+            },
+            createImage(file) {
+                let reader = new FileReader();
+                let vm = this;
+                reader.onload = (e) => {
+                    vm.image = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            },
+            uploadImage(){
+                axios.post('/file/store',{image: this.image}).then(response => {
+                   if (response.data.success) {
+                     alert(response.data.success);
+                   }
+                });
+            }
+        }
     }
 </script>
