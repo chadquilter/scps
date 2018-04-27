2fb013b41e9fe1930c09cd93a76bd278b65b01bf
Chad Quilter <chadquilter@hotmail.com>
2018-04-27 05:07:31 -0500

    add file upload vue component

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,7 +8,19 @@ class UploadController extends Controller
 {
   public function store(Request $request)
   {
+    if($request->get('image'))
+    {
+       $image = $request->get('image');
+       $name = time().'.' . explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];
+       \Image::make($request->get('image'))->save(public_path('images/').$name);
+     }
 
+    $image= new FileUpload();
+    $image->image_name = $name;
+    $image->image_active = 'Y';
+    $image->save();
+
+    return response()->json(['success' => 'You have successfully uploaded an image'], 200);
   }
 
 }
