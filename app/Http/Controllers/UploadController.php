ccd3afc91b6fa309c03cc469538e966efb7dd680
Chad Quilter <chadquilter@gmail.com>
2018-04-27 21:27:56 -0500

    fix missing closing bracket in controller

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -10,23 +10,24 @@ use Storage;
 
 class UploadController extends Controller
 {
-
-  public function store(Request $request){
+  public function store(Request $request)
+  {
     if ($request->hasFile('image')) {
        //$name = time().'.' . explode('/', explode(':', substr($file, 0, strpos($file, ';')))[1])[1];
        //\Image::make($request->get('image'))->save(public_path('storage/app/public/').$file);
        //$destinationPath = 'images/uploads';
-       //$request->file('image')->move($destinationPath,$file->getClientOriginalName());
+       //$file->move($destinationPath,$file->getClientOriginalName());
        //$request->file('image')->store('docs');
-       $request->file('image')->store('public');
-     }
+      $request->file('image')->store('images');
+    }else{
+      return response()->json(['failed' => 'The image was not found in the post data.'], 200);
+    }
 
     $image= new FileUpload();
     $image->image_name = $name;
     $image->image_active = 'Y';
     $image->save();
 
-    return response()->json(['success' => 'You have successfully uploaded an image'], 200);
-  }
-
+      return response()->json(['success' => 'You have successfully uploaded an image'], 200);
+    }
 }
