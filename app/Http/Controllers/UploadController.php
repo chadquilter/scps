01646d4183c0d035aec68c17f5f3ea507ca1796b
Chad Quilter <chadquilter@hotmail.com>
2018-04-27 23:25:26 -0500

    add file upload vue component

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -18,7 +18,7 @@ class UploadController extends Controller
        //$destinationPath = 'images/uploads';
        //$file->move($destinationPath,$file->getClientOriginalName());
        //$request->file('image')->store('docs');
-      /$request->file('image')->store('public/test');
+       $request->file('image')->store('public/test');
       //$request->image->storeAs('public', $request->image->getClientOriginalName());
     }else{
       //return response()->json(['failed' => 'The image was not found in the post data.'], 500);
