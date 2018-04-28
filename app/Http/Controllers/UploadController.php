a4b5793e08948bb9eab3c15c0aacb7fec95b3d8a
Chad Quilter <chadquilter@hotmail.com>
2018-04-27 23:51:47 -0500

    add file upload vue component

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -13,22 +13,23 @@ class UploadController extends Controller
   public function store(Request $request)
   {
     if ($request->hasFile('image')) {
-       //$name = time().'.' . explode('/', explode(':', substr($file, 0, strpos($file, ';')))[1])[1];
+       $name = $request->file('image');
+       $name = time().'.' . explode('/', explode(':', substr($file, 0, strpos($file, ';')))[1])[1];
        //\Image::make($request->get('image'))->save(public_path('storage/app/public/').$file);
        //$destinationPath = 'images/uploads';
        //$file->move($destinationPath,$file->getClientOriginalName());
        //$request->file('image')->store('docs');
-       $request->file('image')->store('public');
-      //$request->image->storeAs('public', $request->image->getClientOriginalName());
+       //$request->file('image')->store('public');
+       $request->image->storeAs('uploads/', $request->image->getClientOriginalName());
 
-      $image = new FileUpload();
-      $image->image_name = $name;
-      $image->image_active = 'Y';
-      $image->save();
+       $image = new FileUpload();
+       $image->image_name = $name;
+       $image->image_active = 'Y';
+       $image->save();
 
-      return response()->json(['success' => 'You have successfully uploaded an image'], 200);
-    }else{
-      return response()->json(['success' => 'not really, your stuff isnt here. Sorry. : (', 200]);
-    }
-  }
-}
+       return response()->json(['success' => 'You have successfully uploaded an image'], 200);
+     }else{
+       return response()->json(['success' => 'not really, your stuff isnt here. Sorry. : (', 200]);
+     }
+   }
+ }
