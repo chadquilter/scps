985f05fc70c120c1df722b3deba76ac21ec2a514
Chad Quilter <chadquilter@hotmail.com>
2018-05-17 23:07:03 -0500

    hide contact form when not on contact page

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -57,12 +57,11 @@
 
               <div class="fb-page" data-href="https://www.facebook.com/spacecityschool" data-tabs="timeline" data-width="250" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/spacecityschool" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/spacecityschool">Space City Preparatory School</a></blockquote></div>
             </div>
-
-            <div class="col col-md-4 col-lg-4 col-xl-4">
               @if(( \Request::url() != url('contact-us') ) && (\Request::url() != url('admission/admission-contact') ) &&  (\Request::url() != url('admission/admission-contact') ) )
-                @include('contact.contactmini')
-              @endif
+            <div class="col col-md-4 col-lg-4 col-xl-4">
+              @include('contact.contactmini')
             </div>
+            @endif
           </div>
         </div>
       </div>
